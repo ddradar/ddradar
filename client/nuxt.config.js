35561aed5c71f6e06b3b791578e53131e1c33403
Nogic <24802730+nogic1008@users.noreply.github.com>
2020-05-28 16:32:18 +0900

    feat: run create-nuxt-app

---
+++ client/nuxt.config.js
@@ -0,0 +1,26 @@
+export default {
+  mode: 'universal',
+  head: {
+    title: 'DDRadar',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      {
+        hid: 'description',
+        name: 'description',
+        content: 'DDR Score Tracker',
+      },
+    ],
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+  },
+  loading: { color: '#fff' },
+  css: [],
+  plugins: [],
+  buildModules: ['@nuxt/typescript-build'],
+  modules: ['nuxt-buefy', '@nuxtjs/axios', '@nuxtjs/pwa'],
+  /*
+   ** Axios module configuration
+   ** See https://axios.nuxtjs.org/options
+   */
+  axios: {},
+}
