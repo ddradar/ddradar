c64e14b0b6f95a8ed1d9502d19d00ba2ebead589
Nogic <24802730+nogic1008@users.noreply.github.com>
2020-05-30 11:57:12 +0900

    feat(api): create getSongInfo API (#5)
    * feat(deps): add @azure/cosmos package
    * feat: fetch data from Cosmos DB
    * chore(deps-dev): add @type/node definition

--- api/getSongInfo/index.ts
+++ api/getSongInfo/index.ts
@@ -1,23 +1,53 @@
+import { CosmosClient } from '@azure/cosmos'
 // eslint-disable-next-line import/no-unresolved, node/no-unpublished-import
 import { AzureFunction, Context, HttpRequest } from '@azure/functions'
 
-const httpTrigger: AzureFunction = async function (
+const httpTrigger: AzureFunction = async (
   context: Context,
   req: HttpRequest
-): Promise<void> {
+): Promise<void> => {
   context.log('HTTP trigger function processed a request.')
-  const name = req.query.name || (req.body && req.body.name)
+  const id = req.query.id
 
-  if (name) {
-    context.res = {
-      // status: 200, /* Defaults to 200 */
-      body: 'Hello ' + (req.query.name || req.body.name),
-    }
-  } else {
+  if (!id) {
     context.res = {
       status: 400,
-      body: 'Please pass a name on the query string or in the request body',
+      body: 'Please pass a id on the query string',
     }
+    return
+  }
+
+  // eslint-disable-next-line node/no-process-env
+  const connectionString = process.env.COSMOS_DB_CONN
+  if (!connectionString) {
+    context.res = {
+      status: 500,
+      body: 'Internal Server Error',
+    }
+    return
+  }
+
+  const client = new CosmosClient(connectionString)
+  const container = client.database('DDRadar').container('Songs')
+
+  const { resources } = await container.items
+    .query({
+      query: 'SELECT * FROM Songs c WHERE c.id = @id',
+      parameters: [{ name: '@id', value: id }],
+    })
+    .fetchAll()
+
+  if (resources.length === 0) {
+    context.res = {
+      status: 404,
+      body: `Not found song that id: "${id}"`,
+    }
+    return
+  }
+
+  context.res = {
+    // status: 200, /* Defaults to 200 */
+    body: resources[0],
   }
 }
 
