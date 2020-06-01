4a869f8f88c6b5812583d8857e0d8731bf484df5
Nogic <24802730+nogic1008@users.noreply.github.com>
2020-06-01 16:06:28 +0900

    fix: change return type to void

--- api/getSongInfo/index.ts
+++ api/getSongInfo/index.ts
@@ -3,37 +3,29 @@ import type { AzureFunction, Context, HttpRequest } from '@azure/functions'
 
 import { SongSchema } from '../song'
 
-type SongInfoResponse =
-  | {
-      status: number
-      body: string
-    }
-  | {
-      status: 200
-      body: SongSchema
-    }
-
 const httpTrigger: AzureFunction = async (
   context: Context,
   req: HttpRequest
-): Promise<SongInfoResponse> => {
+): Promise<void> => {
   context.log('HTTP trigger function processed a request.')
   const id = req.query.id
 
   if (!id) {
-    return {
+    context.res = {
       status: 400,
       body: 'Please pass a id on the query string',
     }
+    return
   }
 
   // eslint-disable-next-line node/no-process-env
   const connectionString = process.env.COSMOS_DB_CONN
   if (!connectionString) {
-    return {
+    context.res = {
       status: 500,
       body: 'Internal Server Error',
     }
+    return
   }
 
   const client = new CosmosClient(connectionString)
@@ -45,13 +37,15 @@ const httpTrigger: AzureFunction = async (
     })
     .fetchAll()
 
-  if (resources.length === 0)
-    return {
+  if (resources.length === 0) {
+    context.res = {
       status: 404,
       body: `Not found song that id: "${id}"`,
     }
+    return
+  }
 
-  return {
+  context.res = {
     status: 200,
     body: resources[0],
   }
