cc2b6c449ab585eb6deb3fe1c564b077616a77d2
nogic <24802730+nogic1008@users.noreply.github.com>
2020-06-08 23:23:50 +0900

    feat(api): add authentication util

---
+++ api/function-utils.ts
@@ -0,0 +1,25 @@
+import type { HttpRequest } from '@azure/functions'
+
+type ClientPricipal = {
+  identityProvider: 'facebook' | 'github' | 'twitter' | 'google' | 'aad'
+  userId: string
+  userDetails: string
+  userRoles: string[]
+}
+
+export const getClientPrincipal = ({
+  headers,
+}: Pick<HttpRequest, 'headers'>): ClientPricipal | null => {
+  const base64String = headers['x-ms-client-principal']
+  if (!base64String) return null
+  const rawByteJson = Buffer.from(base64String, 'base64')
+  return JSON.parse(rawByteJson.toString('utf8'))
+}
+
+export const hasRole = (
+  req: Pick<HttpRequest, 'headers'>,
+  roleName: string
+): boolean => getClientPrincipal(req)?.userRoles.includes(roleName)
+
+export const getUserId = (req: Pick<HttpRequest, 'headers'>): string | null =>
+  getClientPrincipal(req)?.userId
