ddef6abec934eae949b7ace54f97a5c9a2c9d097
Nogic <24802730+nogic1008@users.noreply.github.com>
2020-06-09 15:48:01 +0900

    feat(api): create Get Course Info API

---
+++ api/getCourseInfo/index.test.ts
@@ -0,0 +1,117 @@
+import type { Context } from '@azure/functions'
+
+import { describeIf } from '../__tests__/util'
+import { getConnectionString, getContainer } from '../cosmos'
+import { CourseSchema } from '../course'
+import getCourseInfo from '.'
+
+describe('GET /api/courses', () => {
+  let context: Context
+
+  beforeEach(() => {
+    context = {
+      bindingData: {},
+    } as Context
+  })
+
+  test('/ returns "404 Not Found"', async () => {
+    // Arrange - Act
+    await getCourseInfo(context)
+
+    // Assert
+    expect(context.res.status).toBe(404)
+  })
+
+  test('/foo returns "404 Not Found"', async () => {
+    // Arrange
+    context.bindingData.id = 'foo'
+
+    // Act
+    await getCourseInfo(context)
+
+    // Assert
+    expect(context.res.status).toBe(404)
+    expect(context.res.body).toBe('Please pass a id like "/api/courses/:id"')
+  })
+
+  describeIf(() => !!getConnectionString())(
+    'Cosmos DB integration test',
+    () => {
+      const container = getContainer('Courses')
+      const course: CourseSchema = {
+        id: 'o1Q8Ol8Dol9b0dllD6P0iPQbIoP666Db',
+        name: '皆伝',
+        orders: [
+          {
+            playStyle: 2,
+            difficulty: 4,
+            level: 19,
+            chartOrder: [
+              {
+                songId: '186dd6DQq891Ib9Ilq8Qbo8lIqb0Qoll',
+                songName: 'Valkyrie dimension',
+                playStyle: 2,
+                difficulty: 4,
+                level: 19,
+              },
+              {
+                songId: 'q6di1DQbi88i9QlPol1iIPbb8lP1qP1b',
+                songName: 'POSSESSION',
+                playStyle: 2,
+                difficulty: 4,
+                level: 19,
+              },
+              {
+                songId: '6bid6d9qPQ80DOqiidQQ891o6Od8801l',
+                songName: 'Over The “Period”',
+                playStyle: 2,
+                difficulty: 4,
+                level: 19,
+              },
+              {
+                songId: '9i0q91lPPiO61b9P891O1i86iOP1I08O',
+                songName: 'EGOISM 440',
+                playStyle: 2,
+                difficulty: 4,
+                level: 19,
+              },
+            ],
+          },
+        ],
+      }
+
+      beforeAll(async () => {
+        await container.items.create(course)
+      })
+
+      test('/00000000000000000000000000000000 returns "404 Not Found"', async () => {
+        // Arrange
+        const id = '00000000000000000000000000000000'
+        context.bindingData.id = id
+
+        // Act
+        await getCourseInfo(context)
+
+        // Assert
+        expect(context.res.status).toBe(404)
+        expect(context.res.body).toBe(`Not found song that id: "${id}"`)
+      })
+
+      test('/o1Q8Ol8Dol9b0dllD6P0iPQbIoP666Db returns "200 OK" with JSON body', async () => {
+        // Arrange
+        context.bindingData.id = course.id
+
+        // Act
+        await getCourseInfo(context)
+
+        // Assert
+        expect(context.res.status).toBe(200)
+        expect(context.res.body).toStrictEqual(course)
+      })
+
+      afterAll(async () => {
+        await container.item(course.id, course.id).delete()
+      })
+    }
+  )
+})
---
+++ api/getCourseInfo/index.ts
@@ -0,0 +1,47 @@
+import type { AzureFunction, Context } from '@azure/functions'
+
+import { getContainer } from '../cosmos'
+import { CourseSchema } from '../course'
+
+/** Get course and orders information that match the specified ID. */
+const httpTrigger: AzureFunction = async (context: Context): Promise<void> => {
+  const id: string = context.bindingData.id
+
+  // In Azure Functions, this function will only be invoked if a valid `id` is passed.
+  // So this check is only used to unit tests.
+  if (!id || !/^[01689bdiloqDIOPQ]{32}$/.test(id)) {
+    context.res = {
+      status: 404,
+      body: 'Please pass a id like "/api/courses/:id"',
+    }
+    return
+  }
+
+  const container = getContainer('Courses', true)
+  const columns: (keyof CourseSchema)[] = ['id', 'name', 'orders']
+  const { resources } = await container.items
+    .query<CourseSchema>({
+      query:
+        `SELECT ${columns.map(col => `c.${col}`).join(', ')} ` +
+        'FROM c ' +
+        'WHERE c.id = @id',
+      parameters: [{ name: '@id', value: id }],
+    })
+    .fetchAll()
+
+  if (resources.length === 0) {
+    context.res = {
+      status: 404,
+      body: `Not found course that id: "${id}"`,
+    }
+    return
+  }
+
+  context.res = {
+    status: 200,
+    headers: { 'Content-type': 'application/json' },
+    body: resources[0],
+  }
+}
+
+export default httpTrigger
