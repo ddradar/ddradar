876ce17c2f7e2093b0f99f33c972f51296e05822
Nogic <24802730+nogic1008@users.noreply.github.com>
2020-06-11 23:20:44 +0900

    feat(api): add Get Course List API (#26)

---
+++ api/getCourseList/index.ts
@@ -0,0 +1,36 @@
+import type { AzureFunction, Context } from '@azure/functions'
+
+import { getContainer } from '../cosmos'
+import { CourseSchema, Order } from '../course'
+
+/** Get course information list. */
+const httpTrigger: AzureFunction = async (
+  context: Pick<Context, 'res'>
+): Promise<void> => {
+  const container = getContainer('Courses', true)
+
+  // Create SQL
+  const courseColumns: (keyof CourseSchema)[] = ['id', 'name']
+  const orderColumns: (keyof Order)[] = ['playStyle', 'difficulty', 'level']
+  const joinColumn: keyof CourseSchema = 'orders'
+
+  const { resources } = await container.items
+    .query<CourseSchema>({
+      query:
+        `SELECT ${courseColumns.map(col => `c.${col}`).join(', ')}, ` +
+        'ARRAY(' +
+        `SELECT ${orderColumns.map(col => `o.${col}`).join(', ')} ` +
+        `FROM o IN c.${joinColumn}` +
+        `) as ${joinColumn} ` +
+        'FROM c',
+    })
+    .fetchAll()
+
+  context.res = {
+    status: 200,
+    headers: { 'Content-type': 'application/json' },
+    body: resources,
+  }
+}
+
+export default httpTrigger
