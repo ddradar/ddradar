f61df397a8bb0dc6cb9ede15a711fec50df5da87
Nogic <24802730+nogic1008@users.noreply.github.com>
2020-06-16 17:19:08 +0900

    feat(core): add score utilities

--- core/__tests__/score.test.ts
+++ core/__tests__/score.test.ts
@@ -1,91 +1,176 @@
-import * as score from '../score'
+import {
+  calcExScore,
+  calcScore,
+  getDanceLevel,
+  isScore,
+  Score,
+  setValidScoreFromChart,
+} from '../score'
 
-describe('./core/score.ts', () => {
-  describe('getDanceLevel()', () => {
+describe('./score.ts', () => {
+  describe('isScore', () => {
+    const validScore: Score = {
+      clearLamp: 2,
+      rank: 'AA+',
+      score: 978800,
+    }
+    test.each([undefined, null, true, 1.5, 'foo', {}])(
+      '(%p) returns false',
+      (obj: unknown) => expect(isScore(obj)).toBe(false)
+    )
     test.each([
-      ['D', 0],
-      ['D+', 550000],
-      ['C-', 590000],
-      ['C', 600000],
-      ['C+', 650000],
-      ['B-', 690000],
-      ['B', 700000],
-      ['B+', 750000],
-      ['A-', 790000],
-      ['A', 800000],
-      ['A+', 850000],
-      ['AA-', 890000],
-      ['AA', 900000],
-      ['AA+', 950000],
-      ['AAA', 990000],
-      ['AAA', 1000000],
-    ])('returns %s if score is %i', (expected, num) => {
-      expect(score.getDanceLevel(num)).toBe(expected)
-    })
-    test.each([-1, 10.5, NaN, Infinity, -Infinity])(
-      'throws error if score is %d',
-      d => {
-        expect(() => score.getDanceLevel(d)).toThrowError('score is invalid')
-      }
+      validScore,
+      { ...validScore, exScore: 334 },
+      { ...validScore, maxCombo: 120 },
+      { ...validScore, exScore: 334, maxCombo: 120 },
+    ])('(%p) returns true', (obj: Score) => expect(isScore(obj)).toBe(true))
+    test.each([
+      { ...validScore, score: Infinity },
+      { ...validScore, score: -1 },
+      { ...validScore, score: 10000000 },
+      { ...validScore, clearLamp: -2 },
+      { ...validScore, clearLamp: 8 },
+      { ...validScore, rank: '-' },
+      { ...validScore, exScore: '334' },
+      { ...validScore, maxCombo: true },
+      { ...validScore, exScore: 334, maxCombo: {} },
+      { ...validScore, exScore: NaN, maxCombo: 120 },
+    ])('(%p) returns false', (obj: unknown) => expect(isScore(obj)).toBe(false))
+  })
+  describe('setValidScoreFromChart', () => {
+    const testChart = { notes: 270, freezeArrow: 20, shockArrow: 10 }
+
+    const mfcScore: Score = {
+      score: 1000000,
+      rank: 'AAA',
+      clearLamp: 7,
+      exScore: 900,
+      maxCombo: 280,
+    }
+    const pfcScore: Score = {
+      score: 999990,
+      rank: 'AAA',
+      clearLamp: 6,
+      exScore: 899,
+      maxCombo: 280,
+    }
+    test.each([
+      [{ clearLamp: 7 } as Partial<Score>, mfcScore],
+      [{ score: 1000000 }, mfcScore],
+      [{ exScore: 900 }, mfcScore],
+      [{ score: 999990 }, pfcScore],
+      [{ exScore: 899 }, pfcScore],
+      [{ score: 999800 }, { ...pfcScore, score: 999800, exScore: 880 }],
+      [
+        { exScore: 880, clearLamp: 6 },
+        { ...pfcScore, score: 999800, exScore: 880 },
+      ],
+    ])(
+      '({ notes: 270, freezeArrow: 20, shockArrow: 10 }, %p) returns %p',
+      (incompleteScore: Partial<Score>, expected: Score) =>
+        expect(
+          setValidScoreFromChart(testChart, incompleteScore)
+        ).toStrictEqual(expected)
+    )
+    test.each([
+      [{ exScore: 901 } as Partial<Score>, /^Invalid Score object: exScore/],
+      [{ exScore: 800 }, /^Cannot guess Score object. set score property/],
+    ])(
+      '({ notes: 270, freezeArrow: 20, shockArrow: 10 }, %p) throws error',
+      (incompleteScore: Partial<Score>, err) =>
+        expect(() =>
+          setValidScoreFromChart(testChart, incompleteScore)
+        ).toThrowError(err)
     )
   })
-  describe('calcScore()', () => {
+  describe('getDanceLevel', () => {
+    test.each([
+      [0, 'D'],
+      [550000, 'D+'],
+      [590000, 'C-'],
+      [600000, 'C'],
+      [650000, 'C+'],
+      [690000, 'B-'],
+      [700000, 'B'],
+      [750000, 'B+'],
+      [790000, 'A-'],
+      [800000, 'A'],
+      [850000, 'A+'],
+      [890000, 'AA-'],
+      [900000, 'AA'],
+      [950000, 'AA+'],
+      [990000, 'AAA'],
+      [1000000, 'AAA'],
+    ])('(%i) returns %s', (num, expected) =>
+      expect(getDanceLevel(num)).toBe(expected)
+    )
+    test.each([
+      -1,
+      10.5,
+      NaN,
+      Infinity,
+      -Infinity,
+      1000010,
+    ])('(%d) throws error', d =>
+      expect(() => getDanceLevel(d)).toThrowError(/^Invalid parameter: score/)
+    )
+  })
+  describe('calcScore', () => {
     test.each([
       [
-        1000000,
         { notes: 270, freezeArrow: 20, shockArrow: 10 },
         270,
         0,
         0,
         0,
         30,
+        1000000,
       ],
       [
-        998000,
         { notes: 270, freezeArrow: 20, shockArrow: 10 },
         70,
         200,
         0,
         0,
         30,
+        998000,
       ],
       [
-        693890,
         { notes: 846, freezeArrow: 12, shockArrow: 0 },
         352,
         171,
         103,
         10,
         11,
+        693890,
       ],
       [
-        995500,
         { notes: 492, freezeArrow: 36, shockArrow: 0 },
         421,
         66,
         5,
         0,
         36,
+        995500,
       ],
     ])(
-      'returns %i if chart is %p and score is {Mv:%i, Pf:%i, Gr:%i, Gd:%i, Ok:%i}',
-      (expected, chart, mv, pf, gr, gd, ok) => {
-        expect(score.calcScore(chart, mv, pf, gr, gd, ok)).toBe(expected)
-      }
+      '(%p, %i, %i, %i, %i, %i) returns %i',
+      (chart, mv, pf, gr, gd, ok, expected) =>
+        expect(calcScore(chart, mv, pf, gr, gd, ok)).toBe(expected)
     )
     test.each([
-      [-1, 0, 0, 0, 0, /invalid marvelous:/],
-      [0, 10.5, 0, 0, 0, /invalid perfect:/],
-      [0, 0, Infinity, 0, 0, /invalid great:/],
-      [0, 0, 0, -Infinity, 0, /invalid good:/],
-      [0, 0, 0, 0, NaN, /invalid ok:/],
-      [201, 0, 0, 0, 0, /judge count\(\d+\) is greater than notes\(200\)/],
-      [200, 0, 0, 0, 21, /ok count\(\d+\) is greater than FA\+SA count\(20\)/],
+      [-1, 0, 0, 0, 0, /^Invalid parameter: marvelous/],
+      [0, 10.5, 0, 0, 0, /^Invalid parameter: perfect/],
+      [0, 0, Infinity, 0, 0, /^Invalid parameter: great/],
+      [0, 0, 0, -Infinity, 0, /^Invalid parameter: good/],
+      [0, 0, 0, 0, NaN, /^Invalid parameter: ok/],
+      [201, 0, 0, 0, 0, /^Invalid parameter: judge count\(\d+\) is greater/],
+      [200, 0, 0, 0, 21, /^Invalid parameter: okCount\(\d+\) is greater/],
     ])(
-      'throws error if {Mv:%d, Pf:%d, Gr:%d, Gd:%d, Ok:%d}',
+      '({ notes: 200, freezeArrow: 20, shockArrow: 0 }, %d, %d, %d, %d, %d) throws error',
       (mv, pf, gr, gd, ok, err) => {
         expect(() =>
-          score.calcScore(
+          calcScore(
             { notes: 200, freezeArrow: 20, shockArrow: 0 },
             mv,
             pf,
@@ -97,28 +182,22 @@ describe('./core/score.ts', () => {
       }
     )
   })
-  describe('calcExScore()', () => {
+  describe('calcExScore', () => {
     test.each([
       [0, 0, 0, 0, 0],
-      [865, 144, 99, 100, 45],
-      [1559, 470, 37, 0, 25],
-    ])(
-      'returns %i if {Mv:%i, Pf:%i, Gr:%i, Ok:%i}',
-      (expected, mv, pf, gr, ok) => {
-        expect(score.calcExScore(mv, pf, gr, ok)).toBe(expected)
-      }
+      [144, 99, 100, 45, 865],
+      [470, 37, 0, 25, 1559],
+    ])('(%i, %i, %i, %i) returns %i', (mv, pf, gr, ok, expected) =>
+      expect(calcExScore(mv, pf, gr, ok)).toBe(expected)
     )
     test.each([
-      [-1, 0, 0, 0, /invalid marvelous:/],
-      [0, 10.5, 0, 0, /invalid perfect:/],
-      [0, 0, Infinity, 0, /invalid great:/],
-      [0, 0, 0, -Infinity, /invalid ok:/],
-      [NaN, NaN, NaN, NaN, /invalid marvelous:/],
-    ])(
-      'throws error if {Mv:%d, Pf:%d, Gr:%d, Ok:%d}',
-      (mv, pf, gr, ok, err) => {
-        expect(() => score.calcExScore(mv, pf, gr, ok)).toThrowError(err)
-      }
+      [-1, 0, 0, 0, /^Invalid parameter: marvelous/],
+      [0, 10.5, 0, 0, /^Invalid parameter: perfect/],
+      [0, 0, Infinity, 0, /^Invalid parameter: great/],
+      [0, 0, 0, -Infinity, /^Invalid parameter: ok/],
+      [NaN, NaN, NaN, NaN, /^Invalid parameter: marvelous/],
+    ])('(%d, %d, %d, %d) throws error', (mv, pf, gr, ok, err) =>
+      expect(() => calcExScore(mv, pf, gr, ok)).toThrowError(err)
     )
   })
 })
