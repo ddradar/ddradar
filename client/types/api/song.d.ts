9156352140da16f0f9a5db35d813b84633f7f2f3
nogic <24802730+nogic1008@users.noreply.github.com>
2020-06-18 09:00:35 +0900

    feat(client): create song detail page (WIP)

---
+++ client/types/api/song.d.ts
@@ -0,0 +1,64 @@
+// Port from /api/db/songs.ts
+
+export type SongInfo = {
+  /**
+   * Song id that depend on official site.
+   * @example `^([01689bdiloqDIOPQ]*){32}$`
+   */
+  id: string
+  name: string
+  /**
+   * Song furigana for sorting.
+   * @example `^([A-Z0-9 .ぁ-んー]*)$`
+   */
+  nameKana: string
+  /**
+   * Index for sorting. Associated with the "Choose by Name" folder.
+   * @example `0`: あ行, `1`: か行, ..., `10`: A, `11`: B, ..., `35`: Z, `36`: 数字・記号
+   */
+  nameIndex: number
+  artist: string
+  /** Series title depend on official site. */
+  series: string
+  /**
+   * Displayed min BPM (Beet Per Minutes).
+   * Set to `null` if not revealed, such as "???".
+   */
+  minBPM: number | null
+  /**
+   * Displayed max BPM (Beet Per Minutes).
+   * Set to `null` if not revealed, such as "???".
+   */
+  maxBPM: number | null
+  charts: StepChart[]
+}
+
+type StepChart = {
+  /** `1`: SINGLE, `2`: DOUBLE */
+  playStyle: 1 | 2
+  /**
+   * `0`: BEGINNER,
+   * `1`: BASIC,
+   * `2`: DIFFICULT,
+   * `3`: EXPERT,
+   * `4`: CHALLENGE
+   */
+  difficulty: 0 | 1 | 2 | 3 | 4
+  level: number
+  /** Normal arrow count. (Jump = 1 count) */
+  notes: number
+  /** Freeze Arrow count. */
+  freezeArrow: number
+  /** Shock Arrow count. */
+  shockArrow: number
+  /** Groove Radar STREAM */
+  stream: number
+  /** Groove Radar VOLTAGE */
+  voltage: number
+  /** Groove Radar AIR */
+  air: number
+  /** Groove Radar FREEZE */
+  freeze: number
+  /** Groove Radar CHAOS */
+  chaos: number
+}
