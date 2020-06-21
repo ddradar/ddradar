4d84af4c80901c391928afe5921992b560ee2c61
nogic <24802730+nogic1008@users.noreply.github.com>
2020-06-21 09:34:48 +0900

    docs(client): add documentation comment

--- client/pages/double/_level.vue
+++ client/pages/double/_level.vue
@@ -44,22 +44,25 @@ import { Component, Vue } from 'nuxt-property-decorator'
 import { ChartInfo } from '~/types/api/song'
 
 @Component
-export default class SongBySeriesPage extends Vue {
+export default class DoubleLevelPage extends Vue {
+  /** Chart list */
   charts: ChartInfo[] = []
 
+  /** level expected [1-20] */
   validate({ params }: Pick<Context, 'params'>) {
     const level = parseInt(params.level, 10)
     return /^\d{1,2}$/.test(params.level) && level >= 1 && level <= 20
   }
 
-  // Get chart list from API
+  /** Get chart list from API */
   async fetch() {
     const i = this.$route.params.level
     const charts = await this.$http.$get<ChartInfo[]>(`/charts/2/${i}`)
     this.charts = charts
   }
 
-  title() {
+  /** Page title */
+  get title() {
     return `DOUBLE ${this.$route.params.level}`
   }
 }
