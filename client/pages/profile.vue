4d26d230733abbcb27750badf6b8e68e8e4f517b
nogic <24802730+nogic1008@users.noreply.github.com>
2020-06-27 23:25:07 +0900

    feat(client): add post button in /profile

--- client/pages/profile.vue
+++ client/pages/profile.vue
@@ -18,7 +18,7 @@
 
     <b-field label="所属地域">
       <b-select v-model="area" placeholder="Select" :disabled="!isNewUser">
-        <option v-for="area in areaList" :key="area.key" :value="area.key">
+        <option v-for="area in areaOptions" :key="area.key" :value="area.key">
           {{ value }}
         </option>
       </b-select>
@@ -39,7 +39,7 @@
     </b-field>
 
     <b-field>
-      <b-button type="is-success">
+      <b-button type="is-success" :disabled="hasError">
         保存
       </b-button>
     </b-field>
@@ -59,13 +59,24 @@ export default class ProfilePage extends Vue implements User {
   code: number
   isPublic: boolean
 
-  readonly areaList = Object.entries(areaList).map(v => ({
+  readonly areaOptions = Object.entries(areaList).map(v => ({
     key: v[0],
     value: v[1],
   }))
 
   get isNewUser() {
     return !this.$accessor.user
+  }
+
+  get hasError() {
+    return (
+      !/^[-a-z0-9_]+$/.test(this.id) ||
+      !this.name ||
+      !Object.keys(areaList).includes(`${this.area}`) ||
+      !Number.isInteger(this.code) ||
+      this.code < 10000000 ||
+      this.code > 99999999
+    )
   }
 
   /** Load user info */
@@ -76,6 +87,17 @@ export default class ProfilePage extends Vue implements User {
     this.area = this.$accessor.user?.area ?? 0
     this.code = this.$accessor.user?.code
     this.isPublic = this.$accessor.user?.isPublic ?? true
+  }
+
+  async save() {
+    const user: User = {
+      id: this.id,
+      name: this.name,
+      area: this.area,
+      isPublic: this.isPublic,
+    }
+    if (this.code) user.code = this.code
+    await this.$accessor.saveUser(user)
   }
 }
 </script>
