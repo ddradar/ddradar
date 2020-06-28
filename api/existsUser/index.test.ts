bc076777775fa7d8017e440b7b6b14b5a4800ec4
nogic <24802730+nogic1008@users.noreply.github.com>
2020-06-28 11:16:14 +0900

    feat(api): add existsUser API

---
+++ api/existsUser/index.test.ts
@@ -0,0 +1,127 @@
+import type { Context } from '@azure/functions'
+import { mocked } from 'ts-jest/utils'
+
+import { describeIf } from '../__tests__/util'
+import { getClientPrincipal } from '../auth'
+import { getConnectionString, getContainer } from '../cosmos'
+import type { UserSchema } from '../db'
+import existsUser from '.'
+
+jest.mock('../auth')
+
+describe('GET /api/v1/users/exists', () => {
+  let context: Pick<Context, 'bindingData'>
+  const req = { headers: {} }
+
+  beforeEach(() => {
+    context = { bindingData: {} }
+    mocked(getClientPrincipal).mockReturnValue({
+      identityProvider: 'github',
+      userDetails: 'new_user',
+      userId: '3',
+      userRoles: ['anonymous', 'authenticated'],
+    })
+  })
+
+  test('returns "401 Unauthorized" if not logged in', async () => {
+    // Arrange
+    mocked(getClientPrincipal).mockReturnValueOnce(null)
+
+    // Act
+    const result = await existsUser(context, req)
+
+    // Assert
+    expect(result.status).toBe(401)
+  })
+
+  test('/ returns "404 Not Found"', async () => {
+    // Arrange - Act
+    const result = await existsUser(context, req)
+
+    // Assert
+    expect(result.status).toBe(404)
+  })
+
+  test.each(['', '\\', 'あ'])('/%s returns "404 Not Found"', async id => {
+    // Arrange
+    context.bindingData.id = id
+
+    // Act
+    const result = await existsUser(context, req)
+
+    // Assert
+    expect(result.status).toBe(404)
+  })
+
+  describeIf(() => !!getConnectionString())(
+    'Cosmos DB integration test',
+    () => {
+      const publicUser: UserSchema = {
+        id: 'public_user',
+        loginId: '1',
+        name: 'Public User',
+        area: 13,
+        code: 10000000,
+        isPublic: true,
+      } as const
+      const privateUser: UserSchema = {
+        id: 'private_user',
+        loginId: '2',
+        name: 'Private User',
+        area: 0,
+        isPublic: false,
+      } as const
+
+      beforeAll(async () => {
+        await getContainer('Users').items.create(publicUser)
+        await getContainer('Users').items.create(privateUser)
+      })
+
+      test('/phantom_user returns "200 OK" with not exists JSON body', async () => {
+        // Arrange
+        const id = 'phantom_user'
+        context.bindingData.id = id
+
+        // Act
+        const result = await existsUser(context, req)
+
+        // Assert
+        expect(result.status).toBe(200)
+        expect(result.body).toStrictEqual({ id, exists: false })
+      })
+
+      test(`/${publicUser.id} returns "200 OK" with exists JSON body`, async () => {
+        // Arrange
+        const id = publicUser.id
+        context.bindingData.id = id
+
+        // Act
+        const result = await existsUser(context, req)
+
+        // Assert
+        expect(result.status).toBe(200)
+        expect(result.body).toStrictEqual({ id, exists: true })
+      })
+
+      test(`/${privateUser.id} returns "200 OK" with exists JSON body`, async () => {
+        // Arrange
+        const id = privateUser.id
+        context.bindingData.id = id
+
+        // Act
+        const result = await existsUser(context, req)
+
+        // Assert
+        expect(result.status).toBe(200)
+        expect(result.body).toStrictEqual({ id, exists: true })
+      })
+
+      afterAll(async () => {
+        await getContainer('Users').item(publicUser.id, publicUser.id).delete()
+        await getContainer('Users')
+          .item(privateUser.id, privateUser.id)
+          .delete()
+      })
+    }
+  )
+})
---
+++ api/existsUser/index.ts
@@ -0,0 +1,48 @@
+import type { Context, HttpRequest } from '@azure/functions'
+
+import { getClientPrincipal } from '../auth'
+import { getContainer } from '../cosmos'
+import type {
+  NotFoundResult,
+  SuccessResult,
+  UnauthenticatedResult,
+} from '../function'
+import type { User } from '../user'
+
+type ExistsUser = Pick<User, 'id'> & {
+  exists: boolean
+}
+
+/** Returns whether the specified user exists. */
+export default async function (
+  context: Pick<Context, 'bindingData'>,
+  req: Pick<HttpRequest, 'headers'>
+): Promise<NotFoundResult | SuccessResult<ExistsUser> | UnauthenticatedResult> {
+  // Authentication Check
+  const clientPrincipal = getClientPrincipal(req)
+  if (!clientPrincipal) return { status: 401 } // This check is only used to unit tests.
+
+  const id: string = context.bindingData.id
+  // In Azure Functions, this function will only be invoked if a valid `id` is passed.
+  // So this check is only used to unit tests.
+  if (!id || !/^[-a-z0-9_]+$/.test(id)) {
+    return { status: 404 }
+  }
+
+  const container = getContainer('Users', true)
+  const { resources } = await container.items
+    .query({
+      query: 'SELECT c.id FROM c WHERE c.id = @id',
+      parameters: [{ name: '@id', value: id }],
+    })
+    .fetchAll()
+
+  return {
+    status: 200,
+    headers: { 'Content-type': 'application/json' },
+    body: {
+      id,
+      exists: !!resources.length,
+    },
+  }
+}
