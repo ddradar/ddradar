262d63f77b294cf62e6da29b3d612538287e3d55
Nogic <24802730+nogic1008@users.noreply.github.com>
2020-06-29 09:10:06 +0900

    feat(client): add caption

--- client/pages/profile.vue
+++ client/pages/profile.vue
@@ -1,26 +1,33 @@
 <template>
   <section class="section">
-    <h1 class="title">User Profile</h1>
+    <b-message v-if="isNewUser" type="is-danger" has-icon>
+      ユーザー登録は、まだ完了していません。引き続き、以下の情報を入力してください。
+    </b-message>
+    <h1 class="title">ユーザー設定</h1>
 
-    <b-field label="ID">
+    <b-field
+      label="ID"
+      message="あなたの情報をDDRadar上で管理する識別子です。登録後の変更はできません。"
+    >
       <b-input
         v-model="id"
-        required
-        pattern="^[-a-z0-9_]+$"
         placeholder="半角英数字, ハイフン, アンダーバー"
         :disabled="!isNewUser"
         :loading="loading"
         :type="type"
         :message="message"
-        @blur="checkDuplicate()"
+        @blur="checkId()"
       />
     </b-field>
 
-    <b-field label="表示名">
+    <b-field label="表示名" message="あなたの名前です。">
       <b-input v-model="name" required />
     </b-field>
 
-    <b-field label="所属地域">
+    <b-field
+      label="所属地域"
+      message="あなたの所属地域です。登録後の変更はできません。"
+    >
       <b-select v-model="area" placeholder="Select" :disabled="!isNewUser">
         <option v-for="area in areaOptions" :key="area.key" :value="area.key">
           {{ area.value }}
@@ -28,7 +35,7 @@
       </b-select>
     </b-field>
 
-    <b-field label="DDR CODE">
+    <b-field label="DDR CODE(任意)">
       <b-input
         v-model.number="code"
         placeholder="10000000"
@@ -38,8 +45,14 @@
       />
     </b-field>
 
-    <b-field>
-      <b-switch v-model="isPublic">スコアを公開する</b-switch>
+    <b-field
+      :message="[
+        'ONにすると、ユーザーページが一般公開され、その間に登録されたスコアはランキングに登録されます。',
+        'OFFにすると、ユーザーページは本人のみ閲覧可能となり、その間に登録したスコアも非公開となります。',
+        '※OFFにしても、すでにランキング登録済みのスコアは非公開になりません。ご注意ください。',
+      ]"
+    >
+      <b-switch v-model="isPublic">公開する</b-switch>
     </b-field>
 
     <b-field>
@@ -64,7 +77,7 @@ export default class ProfilePage extends Vue implements User {
   isPublic: boolean = true
 
   loading = false
-  type = ''
+  type: '' | 'is-success' | 'is-error' = ''
   message = ''
 
   readonly areaOptions = Object.entries(areaList).map(v => ({
@@ -78,6 +91,7 @@ export default class ProfilePage extends Vue implements User {
 
   get hasError() {
     return (
+      this.type === 'is-error' ||
       !/^[-a-z0-9_]+$/.test(this.id) ||
       !this.name ||
       !Object.keys(areaList).includes(`${this.area}`) ||
@@ -98,12 +112,29 @@ export default class ProfilePage extends Vue implements User {
     this.isPublic = this.$accessor.user?.isPublic ?? true
   }
 
-  async checkDuplicate() {
+  async checkId() {
+    // Requred check
+    if (!this.id) {
+      this.type = 'is-error'
+      this.message = 'ユーザーIDは必須です'
+      return
+    }
+
+    // Pattern check
+    if (!/^[-a-z0-9_]+$/.test(this.id)) {
+      this.type = 'is-error'
+      this.message =
+        'ユーザーIDは半角英数字, ハイフン, アンダーバーのみ使用可能です'
+      return
+    }
+
+    // Duplicate check from API
     this.loading = true
     const { exists } = await this.$http.$get<{ exists: boolean }>(
       `/api/v1/users/exists/${this.id}`
     )
     this.loading = false
+
     if (exists) {
       this.type = 'is-error'
       this.message = 'ユーザーIDはすでに使われています'
@@ -137,6 +168,8 @@ export default class ProfilePage extends Vue implements User {
         hasIcon: true,
       })
     }
+    this.type = ''
+    this.message = ''
   }
 }
 </script>
