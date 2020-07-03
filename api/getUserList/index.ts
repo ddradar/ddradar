1dcc23b0d75059e244e3902e1926176ded674997
Nogic <24802730+nogic1008@users.noreply.github.com>
2020-07-03 20:04:02 +0900

    fix(api): avoid use continuation token (#113)
    * fix(api): change getUserList API surface
    * fix(client): remove backend-pagination

--- api/getUserList/index.ts
+++ api/getUserList/index.ts
@@ -4,26 +4,20 @@ import type { HttpRequest } from '@azure/functions'
 import { getClientPrincipal } from '../auth'
 import { getContainer } from '../cosmos'
 import { areaCodeList } from '../db/users'
-import type { NotFoundResult, SuccessResult } from '../function'
+import type { SuccessResult } from '../function'
 import type { User } from '../user'
-
-export type UserListResponse = {
-  next: string | null
-  result: User[]
-}
 
 /** Get user list that match the specified conditions. */
 export default async function (
   _context: unknown,
   req: Pick<HttpRequest, 'headers' | 'query'>
-): Promise<NotFoundResult | SuccessResult<UserListResponse>> {
+): Promise<SuccessResult<User[]>> {
   const clientPrincipal = getClientPrincipal(req)
   const loginId = clientPrincipal?.userId ?? ''
 
   const area = parseFloat(req.query.area)
   const name = req.query.name
   const code = parseFloat(req.query.code)
-  const token = req.query.token
 
   // Create SQL WHERE condition dynamically
   const conditions: string[] = ['(c.isPublic = true OR c.loginId = @loginId)']
@@ -42,31 +36,19 @@ export default async function (
   }
 
   const container = getContainer('Users', true)
-  const { resources, continuationToken, hasMoreResults } = await container.items
-    .query<User>(
-      {
-        query:
-          'SELECT c.id, c.name, c.area, c.code ' +
-          'FROM c ' +
-          `WHERE ${conditions.join(' AND ')}`,
-        parameters,
-      },
-      { maxItemCount: 50, continuationToken: token }
-    )
+  const { resources } = await container.items
+    .query<User>({
+      query:
+        'SELECT c.id, c.name, c.area, c.code ' +
+        'FROM c ' +
+        `WHERE ${conditions.join(' AND ')}`,
+      parameters,
+    })
     .fetchNext()
-
-  const queries = parameters
-    .map(p => p.name.replace('@', '&') + p.value?.toString())
-    .join('')
 
   return {
     status: 200,
     headers: { 'Content-type': 'application/json' },
-    body: {
-      next: hasMoreResults
-        ? `/api/v1/users?token=${continuationToken}${queries}`
-        : null,
-      result: resources,
-    },
+    body: resources,
   }
 }
