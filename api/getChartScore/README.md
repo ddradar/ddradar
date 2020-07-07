c48045c3c77bd0f30e7e80ab16d9607abe3aa0ab
Nogic <24802730+nogic1008@users.noreply.github.com>
2020-07-07 11:11:47 +0900

    feat(api): add Get Chart Score API (#123)
    * feat(db): add `id` property in Scores collection
    * fix(db): add compositeIndex in Scores collection
    * feat(api): add Get Chart Score API
    * docs(api): add Get Chart Score API docs
    * test(api): add Chart Score API test

---
+++ api/getChartScore/README.md
@@ -0,0 +1,98 @@
+# Get Chart Score API
+
+日本語版は[こちら](./README-ja.md)にあります。
+
+Get scores that match the specified chart.
+
+- [Endpoint](#endpoint)
+- [Parameters](#parameters)
+- [Response](#response)
+  - [Response Body](#response-body)
+- Links
+  - [Implements (index.ts)](index.ts)
+  - [Settings (function.json)](function.json)
+  - [Unit Test (index.test.ts)](index.test.ts)
+
+## Endpoint
+
+No need Authentication. Authenticated users can get their own data even if they are private.
+
+> GET api/v1/scores/*:songId*/*:playStyle*/*:difficulty*?scope=full
+
+## Parameters
+
+|Name|Type|Description|
+|----|:--:|-----------|
+|`songId`|string|Song ID. Should match `^[01689bdiloqDIOPQ]{32}$` pattern.|
+|`playStyle`|integer|`1`: SINGLE, `2`: DOUBLE|
+|`difficulty`|integer|`0`: BEGINNER, `1`: BASIC, `2`: DIFFICULT, `3`: EXPERT, `4`: CHALLENGE|
+|`scope`|string?|`private`: Only personal best score `medium`(default): Personal best, area top, and world top scores `full`: All scores|
+
+## Response
+
+- Returns `404 Not Found` if parameters are invalid.
+- Returns `404 Not Found` if no score that matches parameters.
+- Returns `200 OK` with [JSON body](#response-body) otherwize.
+
+### Response Body
+
+<details>
+  <summary>Sample</summary>
+
+```json
+[
+  {
+    "userId": "0",
+    "userName": "全国トップ",
+    "songId": "QPd01OQqbOIiDoO1dbdo1IIbb60bqPdl",
+    "songName": "愛言葉",
+    "playStyle": 1,
+    "difficulty": 0,
+    "score": 1000000,
+    "exScore": 402,
+    "maxCombo": 122,
+    "clearLamp": 7,
+    "rank": "AAA"
+  },
+  {
+    "userId": "13",
+    "userName": "東京都トップ",
+    "songId": "QPd01OQqbOIiDoO1dbdo1IIbb60bqPdl",
+    "songName": "愛言葉",
+    "playStyle": 1,
+    "difficulty": 0,
+    "score": 999980,
+    "exScore": 400,
+    "maxCombo": 122,
+    "clearLamp": 6,
+    "rank": "AAA"
+  },
+  {
+    "userId": "public_user",
+    "userName": "AFRO",
+    "songId": "QPd01OQqbOIiDoO1dbdo1IIbb60bqPdl",
+    "songName": "愛言葉",
+    "playStyle": 1,
+    "difficulty": 0,
+    "score": 999950,
+    "clearLamp": 6,
+    "rank": "AAA"
+  }
+]
+```
+
+</details>
+
+|Name|Type|Description|
+|----|:--:|-----------|
+|`userId`|string|User ID|
+|`userName`|string|User name|
+|`songId`|string|Song id that depend on official site. `^([01689bdiloqDIOPQ]*){32}$`|
+|`songName`|string|Song name|
+|`playStyle`|integer|`1`: SINGLE, `2`: DOUBLE|
+|`difficulty`|integer|`0`: BEGINNER, `1`: BASIC, `2`: DIFFICULT, `3`: EXPERT, `4`: CHALLENGE|
+|`score`|integer|Normal score|
+|`exScore`|integer?|EX SCORE (optional)|
+|`maxCombo`|integer?|MAX COMBO (optional)|
+|`clearLamp`|integer|`0`: Failed, `1`: Assisted Clear `2`: Clear, `3`: LIFE4, `4`: Good FC (Full Combo), `5`: Great FC, `6`: PFC, `7`: MFC|
+|`rank`|string|Clear rank (`E`～`AAA`)|
---
+++ api/getChartScore/index.ts
@@ -0,0 +1,122 @@
+import type { SqlParameter } from '@azure/cosmos'
+import type { Context, HttpRequest } from '@azure/functions'
+
+import { getClientPrincipal } from '../auth'
+import { getContainer } from '../cosmos'
+import type { ScoreSchema, UserSchema } from '../db'
+import type { NotFoundResult, SuccessResult } from '../function'
+
+type Score = Omit<ScoreSchema, 'id' | 'isPublic'>
+type Scope = 'private' | 'medium' | 'full'
+
+/** Get course and orders information that match the specified ID. */
+export default async function (
+  context: Pick<Context, 'bindingData'>,
+  req: Pick<HttpRequest, 'headers' | 'query'>
+): Promise<NotFoundResult | SuccessResult<Score[]>> {
+  const clientPrincipal = getClientPrincipal(req)
+
+  const songId: string = context.bindingData.songId
+  const playStyle: number = context.bindingData.playStyle
+  const difficulty: number =
+    typeof context.bindingData.difficulty === 'number'
+      ? context.bindingData.difficulty
+      : 0 // if param is 0, passed object. (bug?)
+  const scope: Scope = ['private', 'medium', 'full'].includes(req.query.scope)
+    ? (req.query.scope as Scope)
+    : 'medium'
+
+  // In Azure Functions, this function will only be invoked if a valid route.
+  // So this check is only used to unit tests.
+  if (
+    !/^[01689bdiloqDIOPQ]{32}$/.test(songId) ||
+    (playStyle !== 1 && playStyle !== 2) ||
+    ![0, 1, 2, 3, 4].includes(difficulty)
+  ) {
+    return { status: 404 }
+  }
+
+  // Create where condition dynamically
+  const conditions: string[] = [
+    'c.songId = @songId',
+    'c.playStyle = @playStyle',
+    'c.difficulty = @difficulty',
+  ]
+  const parameters: SqlParameter[] = [
+    { name: '@songId', value: songId },
+    { name: '@playStyle', value: playStyle },
+    { name: '@difficulty', value: difficulty },
+  ]
+
+  // Get login user info
+  let foundUser = false
+  if (clientPrincipal) {
+    const container = getContainer('Users', true)
+    const { resources } = await container.items
+      .query<UserSchema>({
+        query: 'SELECT * FROM c WHERE c.loginId = @loginId',
+        parameters: [{ name: '@loginId', value: clientPrincipal.userId }],
+      })
+      .fetchAll()
+
+    if (resources.length !== 0) {
+      foundUser = true
+      parameters.push({
+        name: '@ids',
+        value:
+          scope === 'private'
+            ? [resources[0].id]
+            : [resources[0].id, '0', `${resources[0].area}`],
+      })
+    }
+  }
+
+  if (!foundUser) {
+    if (scope === 'private') return { status: 404 }
+    parameters.push({ name: '@ids', value: ['0'] })
+  }
+
+  conditions.push(
+    scope === 'full'
+      ? '(c.isPublic = true OR ARRAY_CONTAINS(@ids, c.userId))'
+      : 'ARRAY_CONTAINS(@ids, c.userId)'
+  )
+
+  const container = getContainer('Scores', true)
+  const columns: (keyof Score)[] = [
+    'userId',
+    'userName',
+    'songId',
+    'songName',
+    'playStyle',
+    'difficulty',
+    'clearLamp',
+    'score',
+    'rank',
+    'exScore',
+    'maxCombo',
+  ]
+  const { resources } = await container.items
+    .query<Score>({
+      query:
+        `SELECT ${columns.map(col => `c.${col}`).join(', ')} ` +
+        'FROM c ' +
+        `WHERE ${conditions.join(' AND ')} ` +
+        'ORDER BY c.score DESC, c.clearLamp DESC, c._ts',
+      parameters,
+    })
+    .fetchAll()
+
+  if (resources.length === 0) {
+    return {
+      status: 404,
+      body: `Not found scores that { songId: "${songId}", playStyle: ${playStyle}, difficulty: ${difficulty} } `,
+    }
+  }
+
+  return {
+    status: 200,
+    headers: { 'Content-type': 'application/json' },
+    body: resources,
+  }
+}
