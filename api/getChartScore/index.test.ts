f949c0301b8bdecfe8d45437f50dcfe7e6e2396c
Nogic <24802730+nogic1008@users.noreply.github.com>
2020-07-07 09:17:47 +0900

    test(api): add integration tests

--- api/getChartScore/index.test.ts
+++ api/getChartScore/index.test.ts
@@ -1,9 +1,13 @@
 import type { Context, HttpRequest } from '@azure/functions'
+import { mocked } from 'ts-jest/utils'
 
 import { describeIf } from '../__tests__/util'
+import { getClientPrincipal } from '../auth'
 import { getConnectionString, getContainer } from '../cosmos'
 import { ScoreSchema, UserSchema } from '../db'
 import getChartScore from '.'
+
+jest.mock('../auth')
 
 describe('GET /api/v1/scores', () => {
   let context: Pick<Context, 'bindingData'>
@@ -72,22 +76,13 @@ describe('GET /api/v1/scores', () => {
     () => {
       const userContainer = getContainer('Users')
       const scoreContainer = getContainer('Scores')
-      const users: UserSchema[] = [
-        {
-          id: 'public_user',
-          loginId: 'public_user',
-          name: 'AFRO',
-          area: 13,
-          isPublic: true,
-        },
-        {
-          id: 'private_user',
-          loginId: 'private_user',
-          name: 'EMI',
-          area: 13,
-          isPublic: false,
-        },
-      ]
+      const user: UserSchema = {
+        id: 'private_user',
+        loginId: 'private_user',
+        name: 'EMI',
+        area: 13,
+        isPublic: false,
+      }
       const scores: ScoreSchema[] = [
         {
           id: '0-06loOQ0DQb0DqbOibl6qO81qlIdoP9DI-1-0',
@@ -157,9 +152,7 @@ describe('GET /api/v1/scores', () => {
       ]
 
       beforeAll(async () => {
-        for (const user of users) {
-          await userContainer.items.create(user)
-        }
+        await userContainer.items.create(user)
         for (const score of scores) {
           await scoreContainer.items.create(score)
         }
@@ -185,10 +178,228 @@ describe('GET /api/v1/scores', () => {
         }
       )
 
+      test('/06loOQ0DQb0DqbOibl6qO81qlIdoP9DI/1/0 returns only World Best Score if anonymous', async () => {
+        // Arrange
+        context.bindingData.songId = '06loOQ0DQb0DqbOibl6qO81qlIdoP9DI'
+        context.bindingData.playStyle = 1
+        context.bindingData.difficulty = 0
+
+        // Act
+        const result = await getChartScore(context, req)
+
+        // Assert
+        expect(result.status).toBe(200)
+        expect(result.body).toStrictEqual([
+          {
+            userId: scores[0].userId,
+            userName: scores[0].userName,
+            songId: '06loOQ0DQb0DqbOibl6qO81qlIdoP9DI',
+            songName: scores[0].songName,
+            playStyle: 1,
+            difficulty: 0,
+            score: scores[0].score,
+            clearLamp: scores[0].clearLamp,
+            rank: scores[0].rank,
+          },
+        ])
+      })
+
+      test('/06loOQ0DQb0DqbOibl6qO81qlIdoP9DI/1/0?scope=medium returns only World Best Score if anonymous', async () => {
+        // Arrange
+        context.bindingData.songId = '06loOQ0DQb0DqbOibl6qO81qlIdoP9DI'
+        context.bindingData.playStyle = 1
+        context.bindingData.difficulty = 0
+        req.query.scope = 'medium'
+
+        // Act
+        const result = await getChartScore(context, req)
+
+        // Assert
+        expect(result.status).toBe(200)
+        expect(result.body).toStrictEqual([
+          {
+            userId: scores[0].userId,
+            userName: scores[0].userName,
+            songId: '06loOQ0DQb0DqbOibl6qO81qlIdoP9DI',
+            songName: scores[0].songName,
+            playStyle: 1,
+            difficulty: 0,
+            score: scores[0].score,
+            clearLamp: scores[0].clearLamp,
+            rank: scores[0].rank,
+          },
+        ])
+      })
+
+      test('/06loOQ0DQb0DqbOibl6qO81qlIdoP9DI/1/0?scope=full returns all public scores if anonymous', async () => {
+        // Arrange
+        context.bindingData.songId = '06loOQ0DQb0DqbOibl6qO81qlIdoP9DI'
+        context.bindingData.playStyle = 1
+        context.bindingData.difficulty = 0
+        req.query.scope = 'full'
+
+        // Act
+        const result = await getChartScore(context, req)
+
+        // Assert
+        expect(result.status).toBe(200)
+        expect(result.body).toStrictEqual([
+          // World Best
+          {
+            userId: scores[0].userId,
+            userName: scores[0].userName,
+            songId: '06loOQ0DQb0DqbOibl6qO81qlIdoP9DI',
+            songName: scores[0].songName,
+            playStyle: 1,
+            difficulty: 0,
+            score: scores[0].score,
+            clearLamp: scores[0].clearLamp,
+            rank: scores[0].rank,
+          },
+          // Public User
+          {
+            userId: scores[2].userId,
+            userName: scores[2].userName,
+            songId: '06loOQ0DQb0DqbOibl6qO81qlIdoP9DI',
+            songName: scores[2].songName,
+            playStyle: 1,
+            difficulty: 0,
+            score: scores[2].score,
+            clearLamp: scores[2].clearLamp,
+            rank: scores[2].rank,
+          },
+        ])
+      })
+
+      test('/06loOQ0DQb0DqbOibl6qO81qlIdoP9DI/1/0 returns World Best, Area Best, and Personal Best Scores if authenticated', async () => {
+        // Arrange
+        mocked(getClientPrincipal).mockReturnValueOnce({
+          identityProvider: 'twitter',
+          userDetails: user.id,
+          userId: user.id,
+          userRoles: ['anonymous', 'authenticated'],
+        })
+        context.bindingData.songId = '06loOQ0DQb0DqbOibl6qO81qlIdoP9DI'
+        context.bindingData.playStyle = 1
+        context.bindingData.difficulty = 0
+
+        // Act
+        const result = await getChartScore(context, req)
+
+        // Assert
+        expect(result.status).toBe(200)
+        expect(result.body).toStrictEqual([
+          // World Best
+          {
+            userId: scores[0].userId,
+            userName: scores[0].userName,
+            songId: '06loOQ0DQb0DqbOibl6qO81qlIdoP9DI',
+            songName: scores[0].songName,
+            playStyle: 1,
+            difficulty: 0,
+            score: scores[0].score,
+            clearLamp: scores[0].clearLamp,
+            rank: scores[0].rank,
+          },
+          // Area Best
+          {
+            userId: scores[1].userId,
+            userName: scores[1].userName,
+            songId: '06loOQ0DQb0DqbOibl6qO81qlIdoP9DI',
+            songName: scores[1].songName,
+            playStyle: 1,
+            difficulty: 0,
+            score: scores[1].score,
+            clearLamp: scores[1].clearLamp,
+            rank: scores[1].rank,
+          },
+          // Personal Best
+          {
+            userId: scores[3].userId,
+            userName: scores[3].userName,
+            songId: '06loOQ0DQb0DqbOibl6qO81qlIdoP9DI',
+            songName: scores[3].songName,
+            playStyle: 1,
+            difficulty: 0,
+            score: scores[3].score,
+            clearLamp: scores[3].clearLamp,
+            rank: scores[3].rank,
+          },
+        ])
+      })
+
+      test('/06loOQ0DQb0DqbOibl6qO81qlIdoP9DI/1/0?scope=full returns Area Best, Personal Best, and all public Scores if authenticated', async () => {
+        // Arrange
+        mocked(getClientPrincipal).mockReturnValueOnce({
+          identityProvider: 'twitter',
+          userDetails: user.id,
+          userId: user.id,
+          userRoles: ['anonymous', 'authenticated'],
+        })
+        context.bindingData.songId = '06loOQ0DQb0DqbOibl6qO81qlIdoP9DI'
+        context.bindingData.playStyle = 1
+        context.bindingData.difficulty = 0
+        req.query.scope = 'full'
+
+        // Act
+        const result = await getChartScore(context, req)
+
+        // Assert
+        expect(result.status).toBe(200)
+        expect(result.body).toStrictEqual([
+          // World Best
+          {
+            userId: scores[0].userId,
+            userName: scores[0].userName,
+            songId: '06loOQ0DQb0DqbOibl6qO81qlIdoP9DI',
+            songName: scores[0].songName,
+            playStyle: 1,
+            difficulty: 0,
+            score: scores[0].score,
+            clearLamp: scores[0].clearLamp,
+            rank: scores[0].rank,
+          },
+          // Area Best
+          {
+            userId: scores[1].userId,
+            userName: scores[1].userName,
+            songId: '06loOQ0DQb0DqbOibl6qO81qlIdoP9DI',
+            songName: scores[1].songName,
+            playStyle: 1,
+            difficulty: 0,
+            score: scores[1].score,
+            clearLamp: scores[1].clearLamp,
+            rank: scores[1].rank,
+          },
+          // Public User
+          {
+            userId: scores[2].userId,
+            userName: scores[2].userName,
+            songId: '06loOQ0DQb0DqbOibl6qO81qlIdoP9DI',
+            songName: scores[2].songName,
+            playStyle: 1,
+            difficulty: 0,
+            score: scores[2].score,
+            clearLamp: scores[2].clearLamp,
+            rank: scores[2].rank,
+          },
+          // Personal Best
+          {
+            userId: scores[3].userId,
+            userName: scores[3].userName,
+            songId: '06loOQ0DQb0DqbOibl6qO81qlIdoP9DI',
+            songName: scores[3].songName,
+            playStyle: 1,
+            difficulty: 0,
+            score: scores[3].score,
+            clearLamp: scores[3].clearLamp,
+            rank: scores[3].rank,
+          },
+        ])
+      })
+
       afterAll(async () => {
-        for (const user of users) {
-          await userContainer.item(user.id, user.id).delete()
-        }
+        await userContainer.item(user.id, user.id).delete()
         for (const score of scores) {
           await scoreContainer.item(score.id, score.userId).delete()
         }
