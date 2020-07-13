cc039449eea73dacf1d4147730c4a2e155cc05a8
Nogic <24802730+nogic1008@users.noreply.github.com>
2020-07-13 09:54:14 +0900

    feat(api): add Delete Chart Score API

---
+++ api/deleteChartScore/index.ts
@@ -0,0 +1,54 @@
+import type { Context, HttpRequest } from '@azure/functions'
+
+import { getClientPrincipal, getLoginUserInfo } from '../auth'
+import { getContainer } from '../cosmos'
+import type {
+  NoContentResult,
+  NotFoundResult,
+  UnauthenticatedResult,
+} from '../function'
+
+/** Get course and orders information that match the specified ID. */
+export default async function (
+  context: Pick<Context, 'bindingData'>,
+  req: Pick<HttpRequest, 'headers'>
+): Promise<NotFoundResult | UnauthenticatedResult | NoContentResult> {
+  const clientPrincipal = getClientPrincipal(req)
+  if (!clientPrincipal) return { status: 401 }
+
+  const songId: string = context.bindingData.songId
+  const playStyle: number = context.bindingData.playStyle
+  const difficulty =
+    typeof context.bindingData.difficulty === 'number'
+      ? context.bindingData.difficulty
+      : 0 // if param is 0, passed object. (bug?)
+
+  // In Azure Functions, this function will only be invoked if a valid route.
+  // So this check is only used to unit tests.
+  if (
+    !/^[01689bdiloqDIOPQ]{32}$/.test(songId) ||
+    (playStyle !== 1 && playStyle !== 2) ||
+    ![0, 1, 2, 3, 4].includes(difficulty)
+  ) {
+    return { status: 404 }
+  }
+
+  const user = await getLoginUserInfo(clientPrincipal)
+  if (!user) {
+    return {
+      status: 404,
+      body: `Unregistered user: { platform: ${clientPrincipal.identityProvider}, id: ${clientPrincipal.userDetails} }`,
+    }
+  }
+
+  const container = getContainer('Scores')
+  const { statusCode } = await container
+    .item(`${user.id}-${songId}-${playStyle}-${difficulty}`, user.id)
+    .delete()
+
+  if (statusCode === 204) {
+    return { status: 204 }
+  }
+
+  return { status: 404 }
+}
