460a47f3883c631505f19d2bb104ce81a7860872
Nogic <24802730+nogic1008@users.noreply.github.com>
2020-07-14 12:28:46 +0900

    refactor(client): use ChartList component

--- client/__tests__/pages/double/_level.test.ts
+++ client/__tests__/pages/double/_level.test.ts
@@ -12,57 +12,6 @@ import { ChartInfo } from '~/types/api/song'
 
 const localVue = createLocalVue()
 localVue.use(Buefy)
-
-const charts: ChartInfo[] = [
-  {
-    id: '9i0q91lPPiO61b9P891O1i86iOP1I08O',
-    name: 'EGOISM 440',
-    series: 'DanceDanceRevolution (2014)',
-    playStyle: 1,
-    difficulty: 4,
-    level: 19,
-  },
-  {
-    id: 'PO9Pl1q896bDDl89qQb98D80DQoPio1I',
-    name: 'ENDYMION',
-    series: 'DanceDanceRevolution A',
-    playStyle: 1,
-    difficulty: 4,
-    level: 19,
-  },
-  {
-    id: '1Dl19idl0i0qiqidbDIIbQddiP6o11PP',
-    name: 'MAX 360',
-    series: 'DanceDanceRevolution A',
-    playStyle: 1,
-    difficulty: 4,
-    level: 19,
-  },
-  {
-    id: '6bid6d9qPQ80DOqiidQQ891o6Od8801l',
-    name: 'Over The “Period”',
-    series: 'DanceDanceRevolution (2014)',
-    playStyle: 1,
-    difficulty: 4,
-    level: 19,
-  },
-  {
-    id: '606b9d6OiliId69bO9Odi6qq8o8Qd0dq',
-    name: 'PARANOiA Revolution',
-    series: 'DDR X3 VS 2ndMIX',
-    playStyle: 1,
-    difficulty: 4,
-    level: 19,
-  },
-  {
-    id: '186dd6DQq891Ib9Ilq8Qbo8lIqb0Qoll',
-    name: 'Valkyrie dimension',
-    series: 'DDR X2',
-    playStyle: 1,
-    difficulty: 4,
-    level: 19,
-  },
-]
 const $fetchState = { pending: false }
 
 describe('/double/_level.vue', () => {
@@ -72,32 +21,74 @@ describe('/double/_level.vue', () => {
       // Arrange
       const wrapper = mount(DoubleLevelPage, {
         localVue,
-        mocks: {
-          $route,
-          $fetchState: { pending: true },
-        },
+        mocks: { $route, $fetchState: { pending: true } },
         stubs: { NuxtLink: RouterLinkStub },
       })
 
       // Act - Assert
-      expect(wrapper.element).toMatchSnapshot()
+      expect(wrapper).toMatchSnapshot()
     })
     test('loaded', () => {
       // Arrange
+      const charts: ChartInfo[] = [
+        {
+          id: '9i0q91lPPiO61b9P891O1i86iOP1I08O',
+          name: 'EGOISM 440',
+          series: 'DanceDanceRevolution (2014)',
+          playStyle: 1,
+          difficulty: 4,
+          level: 19,
+        },
+        {
+          id: 'PO9Pl1q896bDDl89qQb98D80DQoPio1I',
+          name: 'ENDYMION',
+          series: 'DanceDanceRevolution A',
+          playStyle: 1,
+          difficulty: 4,
+          level: 19,
+        },
+        {
+          id: '1Dl19idl0i0qiqidbDIIbQddiP6o11PP',
+          name: 'MAX 360',
+          series: 'DanceDanceRevolution A',
+          playStyle: 1,
+          difficulty: 4,
+          level: 19,
+        },
+        {
+          id: '6bid6d9qPQ80DOqiidQQ891o6Od8801l',
+          name: 'Over The “Period”',
+          series: 'DanceDanceRevolution (2014)',
+          playStyle: 1,
+          difficulty: 4,
+          level: 19,
+        },
+        {
+          id: '606b9d6OiliId69bO9Odi6qq8o8Qd0dq',
+          name: 'PARANOiA Revolution',
+          series: 'DDR X3 VS 2ndMIX',
+          playStyle: 1,
+          difficulty: 4,
+          level: 19,
+        },
+        {
+          id: '186dd6DQq891Ib9Ilq8Qbo8lIqb0Qoll',
+          name: 'Valkyrie dimension',
+          series: 'DDR X2',
+          playStyle: 1,
+          difficulty: 4,
+          level: 19,
+        },
+      ]
       const wrapper = mount(DoubleLevelPage, {
         localVue,
-        mocks: {
-          $route,
-          $fetchState,
-        },
+        mocks: { $route, $fetchState },
         stubs: { NuxtLink: RouterLinkStub },
-        data: () => {
-          return { charts }
-        },
+        data: () => ({ charts }),
       })
 
       // Act - Assert
-      expect(wrapper.element).toMatchSnapshot()
+      expect(wrapper).toMatchSnapshot()
     })
   })
   describe('validate()', () => {
@@ -181,67 +172,6 @@ describe('/double/_level.vue', () => {
       // Act - Assert
       // @ts-ignore
       expect(wrapper.vm.title).toBe(expected)
-    })
-  })
-  describe('shortSeriesName', () => {
-    const wrapper = shallowMount(DoubleLevelPage, {
-      localVue,
-      mocks: {
-        $route: { params: { level: '19' } },
-        $fetchState,
-      },
-      stubs: { NuxtLink: RouterLinkStub },
-    })
-
-    test.each([
-      ['DDR 1st', '1st'],
-      ['DDRMAX', 'DDRMAX'],
-      ['DanceDanceRevolution (2014)', '2014'],
-      ['DanceDanceRevolution A20', 'A20'],
-      ['DanceDanceRevolution A20 PLUS', 'A20 PLUS'],
-    ])('(%s) returns %s', (series, expected) => {
-      // Arrange - Act
-      // @ts-ignore
-      const result = wrapper.vm.shortSeriesName(series)
-
-      // Assert
-      expect(result).toBe(expected)
-    })
-  })
-  describe('getDifficultyName', () => {
-    const wrapper = shallowMount(DoubleLevelPage, {
-      localVue,
-      mocks: {
-        $route: { params: { level: '19' } },
-        $fetchState,
-      },
-      stubs: { NuxtLink: RouterLinkStub },
-    })
-
-    test.each([NaN, -1, 1.5, 5, Infinity, -Infinity])(
-      '(%d) returns "???"',
-      difficulty => {
-        // Arrange - Act
-        // @ts-ignore
-        const result = wrapper.vm.getDifficultyName(difficulty)
-
-        // Assert
-        expect(result).toBe('???')
-      }
-    )
-    test.each([
-      [0, 'BEGINNER'],
-      [1, 'BASIC'],
-      [2, 'DIFFICULT'],
-      [3, 'EXPERT'],
-      [4, 'CHALLENGE'],
-    ])('(%d) returns %s', (difficulty, expected) => {
-      // Arrange - Act
-      // @ts-ignore
-      const result = wrapper.vm.getDifficultyName(difficulty)
-
-      // Assert
-      expect(result).toBe(expected)
     })
   })
 })
