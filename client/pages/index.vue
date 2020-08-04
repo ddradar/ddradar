8fc37ed10f697c7518b5b8cf668af7990379ac7f
Nogic <24802730+nogic1008@users.noreply.github.com>
2020-08-04 11:37:55 +0900

    feat(client): add card to IndexPage

--- client/pages/index.vue
+++ client/pages/index.vue
@@ -8,13 +8,74 @@
         </div>
       </div>
     </section>
-    <section class="section"></section>
+    <section class="section">
+      <div class="content columns is-multiline">
+        <card
+          v-for="m in menuList"
+          :key="m.label"
+          :title="m.label"
+          type="is-primary"
+          collapsible
+          class="column is-half-tablet is-one-third-desktop is-one-quarter-widescreen"
+        >
+          <div class="card-content">
+            <div class="buttons">
+              <b-button
+                v-for="i in m.items"
+                :key="i.name"
+                type="is-text"
+                tag="nuxt-link"
+                :to="i.to"
+              >
+                {{ i.name }}
+              </b-button>
+            </div>
+          </div>
+        </card>
+      </div>
+    </section>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'nuxt-property-decorator'
 
-@Component
-export default class IndexPage extends Vue {}
+import Card from '~/components/shared/Card.vue'
+import { NameIndexList, SeriesList } from '~/types/api/song'
+
+@Component({ components: { Card } })
+export default class IndexPage extends Vue {
+  get menuList() {
+    return [
+      {
+        label: '曲名から探す',
+        items: NameIndexList.map((s, i) => ({
+          name: s,
+          to: `/name/${i}`,
+        })),
+      },
+      {
+        label: 'SINGLEのレベルから探す',
+        items: [...Array(19).keys()].map(i => ({
+          name: `${i + 1}`,
+          to: `/single/${i + 1}`,
+        })),
+      },
+      {
+        label: 'DOUBLEのレベルから探す',
+        items: [...Array(19).keys()].map(i => ({
+          name: `${i + 1}`,
+          to: `/double/${i + 1}`,
+        })),
+      },
+      {
+        label: 'シリーズから探す',
+        items: SeriesList.map((name, i) => ({
+          name,
+          to: `/series/${i}`,
+        })).reverse(),
+      },
+    ]
+  }
+}
 </script>
