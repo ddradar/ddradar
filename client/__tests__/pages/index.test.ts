3e27c86a89a551896a3bf3cd2875692701d0cdf8
nogic <24802730+nogic1008@users.noreply.github.com>
2020-08-13 14:26:22 +0900

    feat(client): add message on top page

--- client/__tests__/pages/index.test.ts
+++ client/__tests__/pages/index.test.ts
@@ -1,16 +1,36 @@
 import { createLocalVue, mount, RouterLinkStub } from '@vue/test-utils'
 import Buefy from 'buefy'
 
+import type { Notification } from '~/api/notification'
 import IndexPage from '~/pages/index.vue'
 
 const localVue = createLocalVue()
 localVue.use(Buefy)
 
-describe('pages/index.vue', () => {
+describe('/pages/index.vue', () => {
   test('renders correctly', () => {
+    const messages: Omit<Notification, 'sender' | 'pinned'>[] = [
+      {
+        id: 'foo',
+        icon: 'account',
+        type: 'is-info',
+        title: 'Title 1',
+        body: 'Message Body1',
+        _ts: 1597244400, // 2020/8/13
+      },
+      {
+        id: 'bar',
+        icon: '',
+        type: 'is-info',
+        title: 'Title 2',
+        body: 'Message Body2',
+        _ts: 1597244400, // 2020/8/13
+      },
+    ]
     const wrapper = mount(IndexPage, {
       localVue,
-      stubs: { NuxtLink: RouterLinkStub },
+      stubs: { NuxtLink: RouterLinkStub, TopMessage: true },
+      data: () => ({ messages }),
     })
     expect(wrapper).toMatchSnapshot()
   })
