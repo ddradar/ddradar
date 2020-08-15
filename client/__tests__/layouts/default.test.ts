e4a33bb1d04cd14fae979e32506641d30d8779ad
nogic <24802730+nogic1008@users.noreply.github.com>
2020-08-15 09:15:51 +0900

    test(client): add fetch() test for DefaultLayout

--- client/__tests__/layouts/default.test.ts
+++ client/__tests__/layouts/default.test.ts
@@ -1,4 +1,9 @@
-import { createLocalVue, mount, RouterLinkStub } from '@vue/test-utils'
+import {
+  createLocalVue,
+  mount,
+  RouterLinkStub,
+  shallowMount,
+} from '@vue/test-utils'
 import Buefy from 'buefy'
 
 import DefaultLayout from '~/layouts/default.vue'
@@ -7,15 +12,13 @@ const localVue = createLocalVue()
 localVue.use(Buefy)
 
 describe('/layouts/default.vue', () => {
+  const stubs = { NuxtLink: RouterLinkStub, Nuxt: true }
   const $fetchState = { pending: false }
   describe('snapshot test', () => {
     test('renders loading', () => {
       const wrapper = mount(DefaultLayout, {
         localVue,
-        stubs: {
-          NuxtLink: RouterLinkStub,
-          Nuxt: true,
-        },
+        stubs,
         mocks: {
           $accessor: { auth: true, user: { name: 'User 1', id: 'user_id' } },
           $fetchState: { pending: true },
@@ -26,10 +29,7 @@ describe('/layouts/default.vue', () => {
     test('renders logout button if authed', () => {
       const wrapper = mount(DefaultLayout, {
         localVue,
-        stubs: {
-          NuxtLink: RouterLinkStub,
-          Nuxt: true,
-        },
+        stubs,
         mocks: {
           $accessor: { auth: true, user: { name: 'User 1', id: 'user_id' } },
           $fetchState,
@@ -40,13 +40,51 @@ describe('/layouts/default.vue', () => {
     test('renders login button if not authed', () => {
       const wrapper = mount(DefaultLayout, {
         localVue,
-        stubs: {
-          NuxtLink: RouterLinkStub,
-          Nuxt: true,
-        },
+        stubs,
         mocks: { $accessor: { auth: false }, $fetchState },
       })
       expect(wrapper).toMatchSnapshot()
+    })
+  })
+  describe('fetch()', () => {
+    const fetchUser = jest.fn()
+    const $router = { push: jest.fn() }
+    beforeEach(() => {
+      fetchUser.mockClear()
+      $router.push.mockClear()
+    })
+    test('calls $accessor.fetchUser()', async () => {
+      // Arrange
+      const $accessor = { fetchUser, auth: true, user: true }
+      const wrapper = shallowMount(DefaultLayout, {
+        localVue,
+        stubs,
+        mocks: { $accessor, $fetchState, $router },
+      })
+
+      // Act
+      await wrapper.vm.$options.fetch?.call(wrapper.vm, null!)
+
+      // Assert
+      expect(fetchUser).toBeCalledTimes(1)
+      expect($router.push).not.toBeCalled()
+    })
+    test('redirects /profile if new user', async () => {
+      // Arrange
+      const $accessor = { fetchUser, auth: true, user: false }
+      const wrapper = shallowMount(DefaultLayout, {
+        localVue,
+        stubs,
+        mocks: { $accessor, $fetchState, $router },
+      })
+
+      // Act
+      await wrapper.vm.$options.fetch?.call(wrapper.vm, null!)
+
+      // Assert
+      expect(fetchUser).toBeCalledTimes(1)
+      expect($router.push).toBeCalledTimes(1)
+      expect($router.push).lastCalledWith('/profile')
     })
   })
 })
