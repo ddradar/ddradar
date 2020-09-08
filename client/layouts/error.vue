7a3f4a8d8c31c555154fb2728b318c9e8249ff49
Nogic <24802730+nogic1008@users.noreply.github.com>
2020-09-08 17:32:53 +0900

    feat(client): translate Error page

--- client/layouts/error.vue
+++ client/layouts/error.vue
@@ -5,9 +5,9 @@
         <div class="container">
           <h1 v-if="statusCode === 404" class="title">
             <b-icon icon="alert" />
-            ページが見つかりません
+            {{ $t('notFound') }}
           </h1>
-          <h1 v-else class="title">エラーが発生しました: {{ statusCode }}</h1>
+          <h1 v-else class="title">{{ $t('otherError') }}{{ statusCode }}</h1>
           <h2 class="subtitle">{{ path }}</h2>
         </div>
       </div>
@@ -19,26 +19,52 @@
     </section>
     <section class="section">
       <div class="container">
-        <nuxt-link to="/"><b-icon icon="home" />Back to Home</nuxt-link>
+        <nuxt-link to="/"><b-icon icon="home" />{{ $t('home') }}</nuxt-link>
       </div>
     </section>
     <footer class="footer">
       <div class="content has-text-centered">
-        <p>
-          このエラーが頻発する場合は、お手数ですが、
-          <a href="https://twitter.com/nogic1008" target="_blank">
-            作者のTwitter
-          </a>
-          または
-          <a href="https://github.com/ddradar/ddradar/issues" target="_blank">
-            Githubのissue
-          </a>
-          にてご報告ください。
-        </p>
+        <i18n path="content.text" tag="p">
+          <template v-slot:twitter>
+            <a href="https://twitter.com/nogic1008" target="_blank">
+              {{ $t('content.text_twitter') }}
+            </a>
+          </template>
+          <template v-slot:github>
+            <a href="https://github.com/ddradar/ddradar/issues" target="_blank">
+              {{ $t('content.text_github') }}
+            </a>
+          </template>
+        </i18n>
       </div>
     </footer>
   </div>
 </template>
+
+<i18n>
+{
+  "ja": {
+    "notFound": "ページが見つかりません",
+    "otherError": "エラーが発生しました: ",
+    "home": "トップページに戻る",
+    "content": {
+      "text": "このエラーが頻発する場合は、お手数ですが、{twitter}または{github}にてご報告ください。",
+      "text_twitter": "作者のTwitter",
+      "text_github": "GitHubのissue"
+    }
+  },
+  "en": {
+    "notFound": "Not Found",
+    "otherError": "Error: ",
+    "home": "Back to Home",
+    "content": {
+      "text": "If this error occurs frequently, please report it on {twitter} or {github}.",
+      "text_twitter": "Twitter",
+      "text_github": "GitHub"
+    }
+  }
+}
+</i18n>
 
 <script lang="ts">
 import { NuxtError } from '@nuxt/types'
