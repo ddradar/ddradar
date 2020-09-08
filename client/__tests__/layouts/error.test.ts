c5adad8efff527daded1748068eb0d9d987feafa
nogic <24802730+nogic1008@users.noreply.github.com>
2020-09-08 23:42:32 +0900

    test(clistn): update error page snapshot

--- client/__tests__/layouts/error.test.ts
+++ client/__tests__/layouts/error.test.ts
@@ -13,25 +13,30 @@ describe('layouts/error.vue', () => {
   const stubs = { NuxtLink: RouterLinkStub }
   test.each(['ja', 'en'])(
     '{ locale: "%s" } renders Not Found error page',
-    () => {
+    locale => {
+      const i18n = new VueI18n({ locale, silentFallbackWarn: true })
       const error: NuxtError = {
         message: 'This page could not be found',
         path: '/foo',
         statusCode: 404,
       }
       const propsData = { error }
-      const wrapper = mount(ErrorPage, { localVue, stubs, propsData })
+      const wrapper = mount(ErrorPage, { localVue, stubs, propsData, i18n })
       expect(wrapper).toMatchSnapshot()
     }
   )
-  test.each(['ja', 'en'])('{ locale: "%s" } renders other error page', () => {
-    const error: NuxtError = {
-      message: 'Internal Server Error',
-      path: '/invalid-call',
-      statusCode: 500,
+  test.each(['ja', 'en'])(
+    '{ locale: "%s" } renders other error page',
+    locale => {
+      const i18n = new VueI18n({ locale, silentFallbackWarn: true })
+      const error: NuxtError = {
+        message: 'Internal Server Error',
+        path: '/invalid-call',
+        statusCode: 500,
+      }
+      const propsData = { error }
+      const wrapper = mount(ErrorPage, { localVue, stubs, propsData, i18n })
+      expect(wrapper).toMatchSnapshot()
     }
-    const propsData = { error }
-    const wrapper = mount(ErrorPage, { localVue, stubs, propsData })
-    expect(wrapper).toMatchSnapshot()
-  })
+  )
 })
