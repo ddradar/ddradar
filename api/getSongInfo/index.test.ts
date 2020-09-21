392d823f3c61636c76edf771c7df781494acebf5
nogic <24802730+nogic1008@users.noreply.github.com>
2020-09-21 22:13:59 +0900

    test(api): use mock on function

--- api/getSongInfo/index.test.ts
+++ api/getSongInfo/index.test.ts
@@ -1,137 +1,67 @@
 import type { Context } from '@azure/functions'
+import { mocked } from 'ts-jest/utils'
 
-import { describeIf } from '../__tests__/util'
-import { canConnectDB, CourseSchema, getContainer, SongSchema } from '../db'
+import { fetchSongInfo, SongSchema } from '../db/songs'
 import getSongInfo from '.'
 
+jest.mock('../db/songs')
+
 describe('GET /api/v1/songs', () => {
-  let context: Pick<Context, 'bindingData'>
+  const context: Pick<Context, 'bindingData'> = { bindingData: {} }
+  const song: SongSchema = {
+    id: '06loOQ0DQb0DqbOibl6qO81qlIdoP9DI',
+    name: 'PARANOiA',
+    nameKana: 'PARANOIA',
+    nameIndex: 25,
+    artist: '180',
+    series: 'DDR 1st',
+    minBPM: 180,
+    maxBPM: 180,
+    charts: [
+      {
+        playStyle: 1,
+        difficulty: 0,
+        level: 4,
+        notes: 138,
+        freezeArrow: 0,
+        shockArrow: 0,
+        stream: 29,
+        voltage: 22,
+        air: 5,
+        freeze: 0,
+        chaos: 0,
+      },
+    ],
+  }
   beforeEach(() => {
-    context = { bindingData: {} }
+    context.bindingData = {}
+    mocked(fetchSongInfo).mockClear()
+    mocked(fetchSongInfo).mockResolvedValue(song)
   })
 
-  describeIf(canConnectDB)('Cosmos DB integration test', () => {
-    const song: SongSchema = {
-      id: '06loOQ0DQb0DqbOibl6qO81qlIdoP9DI',
-      name: 'PARANOiA',
-      nameKana: 'PARANOIA',
-      nameIndex: 25,
-      artist: '180',
-      series: 'DDR 1st',
-      minBPM: 180,
-      maxBPM: 180,
-      charts: [
-        {
-          playStyle: 1,
-          difficulty: 0,
-          level: 4,
-          notes: 138,
-          freezeArrow: 0,
-          shockArrow: 0,
-          stream: 29,
-          voltage: 22,
-          air: 5,
-          freeze: 0,
-          chaos: 0,
-        },
-      ],
-    }
-    const course: CourseSchema = {
-      id: 'o1Q8Ol8Dol9b0dllD6P0iPQbIoP666Db',
-      name: '皆伝',
-      nameIndex: -2,
-      nameKana: '2-11',
-      series: 'DanceDanceRevolution A20',
-      minBPM: 23,
-      maxBPM: 840,
-      charts: [
-        {
-          playStyle: 2,
-          difficulty: 4,
-          level: 19,
-          notes: 634 + 640 + 759 + 804,
-          freezeArrow: 45 + 10 + 28 + 1,
-          shockArrow: 0,
-          order: [
-            {
-              songId: '186dd6DQq891Ib9Ilq8Qbo8lIqb0Qoll',
-              songName: 'Valkyrie dimension',
-              playStyle: 2,
-              difficulty: 4,
-              level: 19,
-            },
-            {
-              songId: 'q6di1DQbi88i9QlPol1iIPbb8lP1qP1b',
-              songName: 'POSSESSION',
-              playStyle: 2,
-              difficulty: 4,
-              level: 19,
-            },
-            {
-              songId: '6bid6d9qPQ80DOqiidQQ891o6Od8801l',
-              songName: 'Over The “Period”',
-              playStyle: 2,
-              difficulty: 4,
-              level: 19,
-            },
-            {
-              songId: '9i0q91lPPiO61b9P891O1i86iOP1I08O',
-              songName: 'EGOISM 440',
-              playStyle: 2,
-              difficulty: 4,
-              level: 19,
-            },
-          ],
-        },
-      ],
-    }
+  test(`/${song.id} returns "200 OK" with JSON`, async () => {
+    // Arrange
+    context.bindingData.id = song.id
 
-    beforeAll(async () => {
-      await getContainer('Songs').items.create(song)
-      await getContainer('Songs').items.create(course)
-    })
+    // Act
+    const result = await getSongInfo(context)
 
-    test('/00000000000000000000000000000000 returns "404 Not Found"', async () => {
-      // Arrange
-      const id = '00000000000000000000000000000000'
-      context.bindingData.id = id
+    // Assert
+    expect(result.status).toBe(200)
+    expect(result.body).toBe(song)
+    expect(mocked(fetchSongInfo)).toBeCalledWith(song.id)
+  })
 
-      // Act
-      const result = await getSongInfo(context)
+  test('/foo returns "404 Not Found" if fetchSongInfo("foo") returns null', async () => {
+    // Arrange
+    context.bindingData.id = 'foo'
+    mocked(fetchSongInfo).mockResolvedValueOnce(null)
 
-      // Assert
-      expect(result.status).toBe(404)
-      expect(result.body).toBe(`Not found song that id: "${id}"`)
-    })
+    // Act
+    const result = await getSongInfo(context)
 
-    test('/o1Q8Ol8Dol9b0dllD6P0iPQbIoP666Db returns "404 Not Found"', async () => {
-      // Arrange
-      const id = course.id
-      context.bindingData.id = id
-
-      // Act
-      const result = await getSongInfo(context)
-
-      // Assert
-      expect(result.status).toBe(404)
-      expect(result.body).toBe(`Not found song that id: "${id}"`)
-    })
-
-    test('/06loOQ0DQb0DqbOibl6qO81qlIdoP9DI returns "200 OK" with JSON body', async () => {
-      // Arrange
-      context.bindingData.id = song.id
-
-      // Act
-      const result = await getSongInfo(context)
-
-      // Assert
-      expect(result.status).toBe(200)
-      expect(result.body).toStrictEqual(song)
-    })
-
-    afterAll(async () => {
-      await getContainer('Songs').item(song.id, song.nameIndex).delete()
-      await getContainer('Songs').item(course.id, course.nameIndex).delete()
-    })
+    // Assert
+    expect(result.status).toBe(404)
+    expect(mocked(fetchSongInfo)).toBeCalledWith('foo')
   })
 })
