6951ada5d214aa5017ba4c7a7ab9b45445e61f44
Nogic <24802730+nogic1008@users.noreply.github.com>
2020-09-22 12:20:30 +0900

    refactor(api): getNotificationInfo

--- api/getNotificationInfo/index.test.ts
+++ api/getNotificationInfo/index.test.ts
@@ -1,91 +1,60 @@
-import { Context } from '@azure/functions'
+import type { Context } from '@azure/functions'
 
-import { describeIf } from '../__tests__/util'
-import { canConnectDB, getContainer, NotificationSchema } from '../db'
+import type { NotificationSchema } from '../db'
 import getNotificationInfo from '.'
 
-describe('GET /api/v1/notification/', () => {
-  let context: Pick<Context, 'bindingData'>
+describe('GET /api/v1/notification/{id}', () => {
+  const context: Pick<Context, 'bindingData'> = { bindingData: {} }
+  const notification: NotificationSchema = {
+    id: 'foo',
+    sender: 'SYSTEM',
+    pinned: false,
+    type: 'is-info',
+    icon: 'info',
+    title: '新曲を追加しました',
+    body: '新曲2曲の譜面情報を追加しました。',
+    timeStamp: 1597028400,
+  }
   beforeEach(() => {
-    context = { bindingData: {} }
+    context.bindingData = {}
   })
 
-  test('/ returns "404 Not Found"', async () => {
-    // Arrange - Act
-    const result = await getNotificationInfo(context)
+  test(`returns "200 OK" with JSON if documents contain 1 notification`, async () => {
+    // Arrange
+    context.bindingData.id = notification.id
+
+    // Act
+    const result = await getNotificationInfo(context, null, [notification])
+
+    // Assert
+    expect(result.status).toBe(200)
+    expect(result.body).toBe(notification)
+  })
+
+  test(`returns "404 Not Found" if documents is []`, async () => {
+    // Arrange
+    context.bindingData.id = 'foo'
+
+    // Act
+    const result = await getNotificationInfo(context, null, [])
 
     // Assert
     expect(result.status).toBe(404)
+    expect(result.body).toMatch(/"foo"/)
   })
 
-  describeIf(canConnectDB)('Cosmos DB integration test', () => {
-    const notification: NotificationSchema[] = [
-      {
-        id: 'foo',
-        sender: 'SYSTEM',
-        pinned: false,
-        type: 'is-info',
-        icon: 'info',
-        title: '新曲を追加しました',
-        body: '新曲2曲の譜面情報を追加しました。',
-        timeStamp: 1597028400,
-      },
-      {
-        id: 'bar',
-        sender: 'SYSTEM',
-        pinned: true,
-        type: 'is-warning',
-        icon: 'warning',
-        title: 'このサイトはベータ版です',
-        body: 'このWebサイトはベータ版環境です。',
-        timeStamp: 1596250800,
-      },
-      {
-        id: 'baz',
-        sender: 'SYSTEM',
-        pinned: false,
-        type: 'is-info',
-        icon: 'info',
-        title: 'v0.6.0をリリースしました',
-        body: '変更点は以下を参照してください。',
-        timeStamp: 1597114800,
-      },
-    ]
-    beforeAll(async () => {
-      await Promise.all(
-        notification.map(d => getContainer('Notification').items.create(d))
-      )
-    })
+  test(`returns "404 Not Found" if documents has 2 or more notification`, async () => {
+    // Arrange
+    context.bindingData.id = 'foo'
 
-    test('/aaa returns "404 Not Found"', async () => {
-      // Arrange
-      context.bindingData.id = 'aaa'
+    // Act
+    const result = await getNotificationInfo(context, null, [
+      notification,
+      notification,
+    ])
 
-      // Act
-      const result = await getNotificationInfo(context)
-
-      // Assert
-      expect(result.status).toBe(404)
-    })
-    test('/foo returns "200 OK" with JSON body', async () => {
-      // Arrange
-      context.bindingData.id = 'foo'
-      const expected = { ...notification[0] }
-
-      // Act
-      const result = await getNotificationInfo(context)
-
-      // Assert
-      expect(result.status).toBe(200)
-      expect(result.body).toStrictEqual(expected)
-    })
-
-    afterAll(async () => {
-      await Promise.all(
-        notification.map(d =>
-          getContainer('Notification').item(d.id, d.sender).delete()
-        )
-      )
-    })
+    // Assert
+    expect(result.status).toBe(404)
+    expect(result.body).toMatch(/"foo"/)
   })
 })
