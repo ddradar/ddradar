b8c8aee9f705b2adcf551c96aa2a39b6ad759a69
Nogic <24802730+nogic1008@users.noreply.github.com>
2020-09-22 12:04:25 +0900

    refactor(api): use binding on getNotificationList

--- api/getNotificationList/index.ts
+++ api/getNotificationList/index.ts
@@ -1,6 +1,6 @@
 import type { HttpRequest } from '@azure/functions'
 
-import { getContainer, NotificationSchema } from '../db'
+import type { NotificationSchema } from '../db/notification'
 import type { SuccessResult } from '../function'
 
 type Notification = Omit<NotificationSchema, 'sender' | 'pinned'>
@@ -8,34 +8,25 @@ type Notification = Omit<NotificationSchema, 'sender' | 'pinned'>
 /** Get system notification list. */
 export default async function (
   _context: unknown,
-  req: Pick<HttpRequest, 'query'>
+  req: Pick<HttpRequest, 'query'>,
+  documents: NotificationSchema[]
 ): Promise<SuccessResult<Notification[]>> {
   const scope = req.query.scope === 'top' ? 'top' : 'full'
 
-  const columns: (keyof Notification)[] = [
-    'id',
-    'type',
-    'icon',
-    'title',
-    'body',
-    'timeStamp',
-  ]
-  const condition = scope === 'top' ? 'WHERE c.pinned = true ' : ''
-  const container = getContainer('Notification')
-  const { resources } = await container.items
-    .query<Notification>({
-      query:
-        'SELECT ' +
-        `${columns.map(c => `c.${c}`).join(', ')} ` +
-        'FROM c ' +
-        condition +
-        'ORDER BY c.pinned, c.timeStamp DESC',
-    })
-    .fetchAll()
+  const body = documents
+    .filter(n => scope === 'full' || n.pinned)
+    .map(n => ({
+      id: n.id,
+      type: n.type,
+      icon: n.icon,
+      title: n.title,
+      body: n.body,
+      timeStamp: n.timeStamp,
+    }))
 
   return {
     status: 200,
     headers: { 'Content-type': 'application/json' },
-    body: resources,
+    body,
   }
 }
