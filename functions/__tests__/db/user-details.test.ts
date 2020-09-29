f89a00bfe29c35befcdedc58a70cd9e6ca7635ad
Nogic <24802730+nogic1008@users.noreply.github.com>
2020-09-29 17:13:34 +0900

    feat(func): add generateGrooveRadar()

--- functions/__tests__/db/user-details.test.ts
+++ functions/__tests__/db/user-details.test.ts
@@ -2,12 +2,16 @@ import type { Container } from '@azure/cosmos'
 import { mocked } from 'ts-jest/utils'
 
 import { getContainer } from '../../db'
-import { fetchGrooveRadar, GrooveRadarSchema } from '../../db/user-details'
+import {
+  fetchGrooveRadar,
+  generateGrooveRadar,
+  GrooveRadarSchema,
+} from '../../db/user-details'
 
 jest.mock('../../db')
 
 describe('/db/user-details.ts', () => {
-  describe('fetchGrooveRadar', () => {
+  describe('fetchGrooveRadar()', () => {
     let resources: (GrooveRadarSchema & { id: string })[] = []
     const radar: GrooveRadarSchema & { id: string } = {
       id: 'foo',
@@ -34,7 +38,7 @@ describe('/db/user-details.ts', () => {
       resources = []
     })
 
-    test('() returns null if not found', async () => {
+    test('returns null if not found', async () => {
       // Arrange
       const userId = 'public_user'
       const playStyle = 2
@@ -46,7 +50,7 @@ describe('/db/user-details.ts', () => {
       expect(container.items.query).toBeCalled()
     })
 
-    test('() returns groove radar if found', async () => {
+    test('returns groove radar if found', async () => {
       // Arrange
       const userId = 'public_user'
       const playStyle = 1
@@ -56,6 +60,60 @@ describe('/db/user-details.ts', () => {
       const result = await fetchGrooveRadar(userId, playStyle)
 
       expect(result).toBe(radar)
+      expect(container.items.query).toBeCalled()
+    })
+  })
+
+  describe('generateGrooveRadar()', () => {
+    let resources: GrooveRadarSchema[] = []
+    const radar: GrooveRadarSchema = {
+      userId: 'public_user',
+      type: 'radar',
+      playStyle: 1,
+      stream: 100,
+      voltage: 100,
+      air: 100,
+      freeze: 100,
+      chaos: 100,
+    }
+
+    const container = {
+      items: {
+        query: jest.fn(() => ({ fetchAll: async () => ({ resources }) })),
+      },
+    }
+    beforeAll(() =>
+      mocked(getContainer).mockReturnValue((container as unknown) as Container)
+    )
+    beforeEach(() => {
+      container.items.query.mockClear()
+      resources = []
+    })
+
+    test('returns groove radar', async () => {
+      // Arrange
+      resources.push(radar)
+
+      // Act
+      const result = await generateGrooveRadar('public_user', 1)
+
+      // Assert
+      expect(result).toBe(radar)
+      expect(container.items.query).toBeCalled()
+    })
+
+    test('() returns empty groove radar if scores is empty', async () => {
+      // Arrange - Act
+      const result = await generateGrooveRadar('public_user', 1)
+
+      expect(result).toStrictEqual({
+        ...radar,
+        stream: 0,
+        voltage: 0,
+        air: 0,
+        freeze: 0,
+        chaos: 0,
+      })
       expect(container.items.query).toBeCalled()
     })
   })
--- functions/db/user-details.ts
+++ functions/db/user-details.ts
@@ -6,6 +6,7 @@ export type GrooveRadarSchema = Pick<
   StepChartSchema,
   'playStyle' | 'stream' | 'voltage' | 'air' | 'freeze' | 'chaos'
 > & {
+  id?: string
   userId: string
   type: 'radar'
 }
@@ -14,6 +15,7 @@ export type ClearStatusSchema = Pick<
   ScoreSchema,
   'userId' | 'playStyle' | 'level' | 'clearLamp'
 > & {
+  id?: string
   type: 'clear'
   count: number
 }
@@ -22,6 +24,7 @@ export type ScoreStatusSchema = Pick<
   ScoreSchema,
   'userId' | 'playStyle' | 'level' | 'rank'
 > & {
+  id?: string
   type: 'score'
   count: number
 }
@@ -29,9 +32,9 @@ export type ScoreStatusSchema = Pick<
 export async function fetchGrooveRadar(
   userId: string,
   playStyle: 1 | 2
-): Promise<(GrooveRadarSchema & { id: string }) | null> {
+): Promise<Required<GrooveRadarSchema> | null> {
   const container = getContainer('UserDetails')
-  const columns: (keyof GrooveRadarSchema | 'id')[] = [
+  const columns: (keyof GrooveRadarSchema)[] = [
     'id',
     'userId',
     'type',
@@ -45,7 +48,7 @@ export async function fetchGrooveRadar(
   const column = columns.map(c => `c.${c}`).join(', ')
 
   const { resources } = await container.items
-    .query<GrooveRadarSchema & { id: string }>({
+    .query<Required<GrooveRadarSchema>>({
       query:
         `SELECT ${column} ` +
         'FROM c ' +
@@ -57,4 +60,47 @@ export async function fetchGrooveRadar(
     })
     .fetchAll()
   return resources[0] ?? null
+}
+
+export async function generateGrooveRadar(
+  userId: string,
+  playStyle: 1 | 2
+): Promise<GrooveRadarSchema> {
+  const container = getContainer('Scores')
+  const columns: (keyof GrooveRadarSchema)[] = [
+    'stream',
+    'voltage',
+    'air',
+    'freeze',
+    'chaos',
+  ]
+  const column = columns.map(c => `MAX(c.radar.${c}) AS ${c}`).join(', ')
+  const { resources } = await container.items
+    .query<GrooveRadarSchema>({
+      query:
+        `SELECT c.userId, "radar" AS type, c.playStyle, ${column} ` +
+        'FROM c ' +
+        'WHERE c.userId = @id ' +
+        'AND c.playStyle = @playStyle ' +
+        'AND IS_DEFINED(c.radar) ' +
+        'AND ((NOT IS_DEFINED(c.ttl)) OR c.ttl = -1 OR c.ttl = null) ' +
+        'GROUP BY c.playStyle',
+      parameters: [
+        { name: '@id', value: userId },
+        { name: '@playStyle', value: playStyle },
+      ],
+    })
+    .fetchAll()
+  return (
+    resources[0] ?? {
+      userId,
+      type: 'radar',
+      playStyle,
+      stream: 0,
+      voltage: 0,
+      air: 0,
+      freeze: 0,
+      chaos: 0,
+    }
+  )
 }
