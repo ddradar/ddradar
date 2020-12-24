2af988b7ec1236f1d45091eefb7984fc407cf7c1
Nogic <24802730+nogic1008@users.noreply.github.com>
2020-12-24 14:35:27 +0900

    refactor(api): rename Search Song by Series API

--- core/api/song.d.ts
+++ core/api/song.d.ts
@@ -2,20 +2,20 @@ import type { SongSchema, StepChartSchema } from '../db/songs'
 
 /**
  * Object type returned by `/api/v1/songs/{:songId}`
- * @see https://github.com/ddradar/ddradar/blob/master/api/getSongInfo
+ * @see https://github.com/ddradar/ddradar/blob/master/api/getSongInfo/
  */
 export type SongInfo = Omit<SongSchema, 'skillAttackId'>
 
 /**
  * Object type returned by `/api/v1/songs/name/{:name}` and `/api/v1/songs/series/{:series}`
- * @see https://github.com/ddradar/ddradar/blob/master/api/searchSongByName
- * @see https://github.com/ddradar/ddradar/blob/master/api/searchSongBySeries
+ * @see https://github.com/ddradar/ddradar/blob/master/api/searchSongByName/
+ * @see https://github.com/ddradar/ddradar/blob/master/api/songs__series__id/
  */
 export type SongListData = Omit<SongSchema, 'charts' | 'skillAttackId'>
 
 /**
  * Object type returned by `/api/v1/charts/{:playStyle}/{:level}`
- * @see https://github.com/ddradar/ddradar/blob/master/api/searchCharts
+ * @see https://github.com/ddradar/ddradar/blob/master/api/searchCharts/
  */
 export type ChartInfo = Pick<SongSchema, 'id' | 'name' | 'series'> &
   Pick<StepChartSchema, 'playStyle' | 'difficulty' | 'level'>
