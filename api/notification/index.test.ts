3234f64fe8f717c7b34d2bf60b5cbb6a5fffb485
Nogic <24802730+nogic1008@users.noreply.github.com>
2021-01-13 14:47:41 +0900

    refactor(api): use query binding

--- api/notification/index.test.ts
+++ api/notification/index.test.ts
@@ -2,9 +2,16 @@ import { notifications } from '../core/__tests__/data'
 import getNotification from '.'
 
 describe('GET /api/v1/notification', () => {
-  const documents = [...notifications]
+  const documents = notifications.map(d => ({
+    id: d.id,
+    type: d.type,
+    icon: d.icon,
+    title: d.title,
+    body: d.body,
+    timeStamp: d.timeStamp,
+  }))
 
-  test('returns "200 OK" with all data', async () => {
+  test('returns "200 OK" with JSON body', async () => {
     // Arrange
     const req = { query: {} }
 
@@ -13,33 +20,6 @@ describe('GET /api/v1/notification', () => {
 
     // Assert
     expect(result.status).toBe(200)
-    expect(result.body).toHaveLength(3)
-  })
-
-  test.each(['full', 'foo'])(
-    '/scope=%s returns "200 OK" with all data',
-    async scope => {
-      // Arrange
-      const req = { query: { scope } }
-
-      // Act
-      const result = await getNotification(null, req, documents)
-
-      // Assert
-      expect(result.status).toBe(200)
-      expect(result.body).toHaveLength(3)
-    }
-  )
-
-  test('?scope=top returns "200 OK" with pinned data', async () => {
-    // Arrange
-    const req = { query: { scope: 'top' } }
-
-    // Act
-    const result = await getNotification(null, req, documents)
-
-    // Assert
-    expect(result.status).toBe(200)
-    expect(result.body).toHaveLength(1)
+    expect(result.body).toBe(documents)
   })
 })
--- api/notification/index.ts
+++ api/notification/index.ts
@@ -1,27 +1,11 @@
-import type { HttpRequest } from '@azure/functions'
-
 import type { NotificationListData } from '../core/api/notification'
-import type { NotificationSchema } from '../core/db/notification'
 import { SuccessResult } from '../function'
 
 /** Get system notification list. */
 export default async function (
   _context: unknown,
-  req: Pick<HttpRequest, 'query'>,
-  documents: NotificationSchema[]
+  _req: unknown,
+  documents: NotificationListData[]
 ): Promise<SuccessResult<NotificationListData[]>> {
-  const scope = req.query.scope === 'top' ? 'top' : 'full'
-
-  const body = documents
-    .filter(n => scope === 'full' || n.pinned)
-    .map(n => ({
-      id: n.id,
-      type: n.type,
-      icon: n.icon,
-      title: n.title,
-      body: n.body,
-      timeStamp: n.timeStamp,
-    }))
-
-  return new SuccessResult(body)
+  return new SuccessResult(documents)
 }
