e8a1f4abceaa962f9f94bb06b859fb0652e83ed4
Nogic <24802730+nogic1008@users.noreply.github.com>
2021-01-14 12:08:44 +0900

    feat(client): add ReactiveDoughnut component

---
+++ client/components/shared/ReactiveDoughnut.ts
@@ -0,0 +1,19 @@
+import { ChartData, ChartOptions } from 'chart.js'
+import { Component, mixins as Mixins, Prop } from 'nuxt-property-decorator'
+import { Doughnut, mixins } from 'vue-chartjs'
+
+@Component
+export default class ReactiveDoughnut extends Mixins(
+  Doughnut,
+  mixins.reactiveProp
+) {
+  @Prop({ type: Object, required: true })
+  readonly chartData!: ChartData
+
+  @Prop({ type: Object, default: null })
+  readonly chartOptions!: ChartOptions
+
+  mounted() {
+    this.renderChart(this.chartData)
+  }
+}
