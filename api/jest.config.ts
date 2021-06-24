71460cb29f1e9c2c1c6f8736eeea78ca73e7423c
Nogic <24802730+nogic1008@users.noreply.github.com>
2021-06-24 14:48:07 +0900

    test(db): set timeout

--- api/jest.config.ts
+++ api/jest.config.ts
@@ -6,11 +6,9 @@ const config: Config.InitialOptions = {
   moduleFileExtensions: ['js', 'ts'],
   testEnvironment: 'node',
   testMatch: ['**/*.test.ts'],
-  testPathIgnorePatterns: ['<rootDir>/core/*'],
   transform: {
     '^.+\\.ts$': 'ts-jest',
   },
-  globalSetup: '<rootDir>/__tests__/initDatabase.js',
   coverageDirectory: './coverage/',
   collectCoverage: true,
   collectCoverageFrom: [
