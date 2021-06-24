53223457062c7f39a9e1f66bf575975d8e3fa971
Nogic <24802730+nogic1008@users.noreply.github.com>
2021-06-24 13:44:41 +0900

    feat(db): ignore deleted song & score on count

--- db/__tests__/songs.test.ts
+++ db/__tests__/songs.test.ts
@@ -36,7 +36,7 @@ describe('songs.ts', () => {
         query:
           'SELECT c.playStyle, c.level, COUNT(1) AS count ' +
           'FROM s JOIN c IN s.charts ' +
-          'WHERE s.nameIndex != -1 AND s.nameIndex != -2 ' +
+          'WHERE s.nameIndex != -1 AND s.nameIndex != -2 AND (s.deleted ?? false != true) ' +
           'GROUP BY c.playStyle, c.level',
       })
     })
--- db/songs.ts
+++ db/songs.ts
@@ -14,7 +14,7 @@ export async function fetchTotalChartCount(): Promise<TotalCount[]> {
       query:
         'SELECT c.playStyle, c.level, COUNT(1) AS count ' +
         'FROM s JOIN c IN s.charts ' +
-        'WHERE s.nameIndex != -1 AND s.nameIndex != -2 ' +
+        'WHERE s.nameIndex != -1 AND s.nameIndex != -2 AND (s.deleted ?? false != true) ' +
         'GROUP BY c.playStyle, c.level',
     })
     .fetchAll()
