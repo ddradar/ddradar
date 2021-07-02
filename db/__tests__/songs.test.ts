ef428f30de9283a5cbf2b76785923b6663830be4
Nogic <24802730+nogic1008@users.noreply.github.com>
2021-07-02 15:12:31 +0900

    test: fix test

--- db/__tests__/songs.test.ts
+++ db/__tests__/songs.test.ts
@@ -36,7 +36,7 @@ describe('songs.ts', () => {
         query:
           'SELECT c.playStyle, c.level, COUNT(1) AS count ' +
           'FROM s JOIN c IN s.charts ' +
-          'WHERE s.nameIndex != -1 AND s.nameIndex != -2 AND (s.deleted ?? false != true) ' +
+          'WHERE s.nameIndex != -1 AND s.nameIndex != -2 AND NOT (IS_DEFINED(s.deleted) AND s.deleted = true) ' +
           'GROUP BY c.playStyle, c.level',
       })
     })
