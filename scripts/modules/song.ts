48abcdc49eaf1b119f26ca7748968ffd826f8e06
nogic <24802730+nogic1008@users.noreply.github.com>
2021-08-02 07:49:37 +0900

    perf(script): cache courseId

--- scripts/modules/song.ts
+++ scripts/modules/song.ts
@@ -1,11 +1,13 @@
 import { fetchList } from '@ddradar/db'
 
+let courseIds: string[] = []
+
 export async function isCourse(songId: string): Promise<boolean> {
-  const courseIds = await fetchList(
-    'Songs',
-    ['id'],
-    [{ condition: 'c.nameIndex < 0' }],
-    { _ts: 'ASC' }
-  )
-  return courseIds.map(d => d.id).includes(songId)
+  if (courseIds.length === 0)
+    courseIds = (
+      await fetchList('Songs', ['id'], [{ condition: 'c.nameIndex < 0' }], {
+        _ts: 'ASC',
+      })
+    ).map(d => d.id)
+  return courseIds.includes(songId)
 }
