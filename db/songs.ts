9b1aa3a65d3d9edb531e6ae72a534183c14f51ef
Nogic <24802730+nogic1008@users.noreply.github.com>
2021-08-06 14:20:55 +0900

    refactor(db): change SQL

--- db/songs.ts
+++ db/songs.ts
@@ -15,7 +15,7 @@ export async function fetchTotalChartCount(): Promise<TotalCount[]> {
       query:
         'SELECT c.playStyle, c.level, COUNT(1) AS count ' +
         'FROM s JOIN c IN s.charts ' +
-        'WHERE s.nameIndex != -1 AND s.nameIndex != -2 AND NOT (IS_DEFINED(s.deleted) AND s.deleted = true) ' +
+        'WHERE s.nameIndex NOT IN (-1, -2) AND NOT (IS_DEFINED(s.deleted) AND s.deleted = true) ' +
         'GROUP BY c.playStyle, c.level',
     })
     .fetchAll()
