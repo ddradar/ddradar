6a545e0cd8e30b6a4f2aec5653e9af533d9ffbbb
nogic <24802730+nogic1008@users.noreply.github.com>
2022-04-24 19:17:43 +0900

    build(web-app): setup oruga-ui

--- web-app/nuxt.config.ts
+++ web-app/nuxt.config.ts
@@ -1,4 +1,19 @@
 import { defineNuxtConfig } from 'nuxt'
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
-export default defineNuxtConfig({})
+export default defineNuxtConfig({
+  app: {
+    head: {
+      link: [
+        {
+          type: 'text/css',
+          href: 'https://cdn.jsdelivr.net/npm/@mdi/font@5.8.55/css/materialdesignicons.min.css',
+          rel: 'preload',
+          as: 'style',
+          onload: "this.rel='stylesheet'",
+        },
+      ],
+    },
+  },
+  css: ['~/assets/css/styles.scss'],
+})
