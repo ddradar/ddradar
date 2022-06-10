9a64314e6e1f9d674680a3f18a23880d06d24eea
Nogic <24802730+nogic1008@users.noreply.github.com>
2022-06-10 01:13:23 +0000

    chore(volar): remove deprecated settings

--- vetur.config.js
+++
@@ -1,8 +0,0 @@
-/** @see https://vuejs.github.io/vetur/reference/#example */
-module.exports = {
-  settings: {
-    'vetur.useWorkspaceDependencies': true,
-    'vetur.experimental.templateInterpolationService': true,
-  },
-  projects: ['./client'],
-}
