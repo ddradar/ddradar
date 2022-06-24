a1f166addc99e9ebedd2f8497bf7d74f4b8041f4
Nogic <24802730+nogic1008@users.noreply.github.com>
2022-06-24 06:50:44 +0000

    feat(web): add Get Song API

---
+++ web/server/api/v2/songs/[id].get.ts
@@ -0,0 +1,79 @@
+import { Api, Song } from '@ddradar/core'
+import { fetchOne } from '@ddradar/db'
+
+export type SongInfo = Api.SongInfo
+
+/**
+ * Get song and charts information that match the specified ID.
+ * @description
+ * - No need Authentication.
+ * - `GET api/v2/songs/:id`
+ *   - `id`: {@link SongInfo.id}
+ * @param _context Azure Functions context (unused)
+ * @param _req HTTP Request (unused)
+ * @param song Song data (from Cosmos DB binding)
+ * @returns
+ * - Returns `400 Bad Request` if {@link SongInfo.id id} is invalid.
+ * - Returns `404 Not Found` if no song that matches {@link SongInfo.id id}.
+ * - Returns `200 OK` with JSON body if found.
+ * @example
+ * ```json
+ * {
+ *   "id": "61oIP0QIlO90d18ObDP1Dii6PoIQoOD8",
+ *   "name": "イーディーエム・ジャンパーズ",
+ *   "nameKana": "いーでぃーえむ じゃんぱーず",
+ *   "nameIndex": 0,
+ *   "artist": "かめりあ feat. ななひら",
+ *   "series": "DanceDanceRevolution A",
+ *   "minBPM": 72,
+ *   "maxBPM": 145,
+ *   "charts": [
+ *     {
+ *       "playStyle": 1,
+ *       "difficulty": 0,
+ *       "level": 3,
+ *       "notes": 70,
+ *       "freezeArrow": 11,
+ *       "shockArrow": 0,
+ *       "stream": 12,
+ *       "voltage": 11,
+ *       "air": 1,
+ *       "freeze": 20,
+ *       "chaos": 0
+ *     }
+ *   ]
+ * }
+ * ```
+ */
+export default defineEventHandler(async event => {
+  const id: unknown = event.context.params.id
+  if (typeof id !== 'string' || Song.isValidId(id)) {
+    event.res.statusCode = 400
+    return null
+  }
+
+  const song = await fetchOne(
+    'Songs',
+    [
+      'id',
+      'name',
+      'nameKana',
+      'nameIndex',
+      'artist',
+      'series',
+      'minBPM',
+      'maxBPM',
+      'deleted',
+      'charts',
+    ],
+    { condition: 'c.id = @', value: id },
+    { condition: 'c.nameIndex != -1' },
+    { condition: 'c.nameIndex != -2' }
+  ) as SongInfo
+
+  if (!song) {
+    event.res.statusCode = 404
+    return null
+  }
+  return song
+})
