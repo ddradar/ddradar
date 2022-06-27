c01a9defb8593e046395a34bf66026a00c9d5228
Nogic <24802730+nogic1008@users.noreply.github.com>
2022-06-27 04:05:06 +0000

    test: fix test case

--- web/__tests__/server/api/user.get.test.ts
+++ web/__tests__/server/api/user.get.test.ts
@@ -9,17 +9,15 @@ import { createEvent } from '../test-util'
 vi.mock('~/server/auth')
 
 describe('GET /api/v1/user', () => {
-  test('returns "404 Not Found" if getLoginUserInfo() returns null', async () => {
+  test('throws Error if getLoginUserInfo() returns null', () => {
     // Arrange
     const event = createEvent()
     vi.mocked(getLoginUserInfo).mockResolvedValueOnce(null)
 
-    // Act
-    const result = await getCurrentUser(event)
-
-    // Assert
-    expect(event.res.statusCode).toBe(404)
-    expect(result).toBe('User registration is not completed')
+    // Act - Assert
+    expect(getCurrentUser(event)).rejects.toThrowError(
+      'User registration is not completed'
+    )
   })
 
   test('returns "200 OK" with JSON body if getLoginUserInfo() returns user', async () => {
