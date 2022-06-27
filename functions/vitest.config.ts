a8b19f82309100a50da151edf78f9e59a77e9319
Nogic <24802730+nogic1008@users.noreply.github.com>
2022-06-27 00:50:30 +0000

    test(func): migrate to vitest

---
+++ functions/vitest.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vitest/config'
+
+export default defineConfig({
+  resolve: {
+    alias: {
+      '@ddradar/core/__tests__/data': '@ddradar/core/__tests__/data.ts',
+      '@ddradar/core': '@ddradar/core/index.ts',
+      '@ddradar/db': '@ddradar/db/index.ts',
+    },
+  },
+  test: {
+    coverage: { enabled: true, reporter: ['json', 'text'] },
+  },
+})
