64e87cc987598abedca85b0e0a478f5b3791aa7a
Nogic <24802730+nogic1008@users.noreply.github.com>
2022-07-18 05:54:46 +0000

    build(web): setup oruga ui

--- web/nuxt.config.ts
+++ web/nuxt.config.ts
@@ -3,4 +3,5 @@ import { defineNuxtConfig } from 'nuxt'
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
   nitro: { preset: 'azure' },
+  css: ['@/assets/css/main.scss'],
 })
