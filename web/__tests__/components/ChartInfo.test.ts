259e5f7aad857d61973f91bef868142b83163f99
Nogic <24802730+nogic1008@users.noreply.github.com>
2022-07-20 02:54:45 +0000

    feat(web): add ChartInfo component

---
+++ web/__tests__/components/ChartInfo.test.ts
@@ -0,0 +1,27 @@
+import { testCourseData, testSongData } from '@ddradar/core/__tests__/data'
+import Oruga from '@oruga-ui/oruga-next'
+import { bulmaConfig } from '@oruga-ui/theme-bulma'
+import { mount, RouterLinkStub } from '@vue/test-utils'
+import { describe, expect, test } from 'vitest'
+
+import ChartInfo from '~/components/ChartInfo.vue'
+
+describe('components/ChartInfo.vue', () => {
+  const testProps = {
+    SongChart: testSongData.charts[0],
+    CourseChart: testCourseData.charts[1],
+  }
+  test.each(['SongChart', 'CourseChart'] as const)(
+    '({ chart: %s }) snapshot test',
+    key => {
+      const wrapper = mount(ChartInfo, {
+        props: { chart: testProps[key] },
+        global: {
+          plugins: [[Oruga, bulmaConfig]],
+          stubs: { NuxtLink: RouterLinkStub },
+        },
+      })
+      expect(wrapper.element).toMatchSnapshot()
+    }
+  )
+})
--- web/plugins/oruga.ts
+++ web/plugins/oruga.ts
@@ -1,6 +1,6 @@
 import Oruga from '@oruga-ui/oruga-next'
 import { bulmaConfig } from '@oruga-ui/theme-bulma'
 
-export default defineNuxtPlugin(nuxtApp => {
-  nuxtApp.vueApp.use(Oruga, bulmaConfig)
+export default defineNuxtPlugin(({ vueApp }) => {
+  vueApp.use(Oruga, bulmaConfig)
 })
