9761355f3b9e89649747433decc15a6caa10ffab
Nogic <24802730+nogic1008@users.noreply.github.com>
2022-07-29 01:10:26 +0000

    feat(web): add menu in top page

--- web/pages/index.vue
+++ web/pages/index.vue
@@ -1,3 +1,45 @@
 <template>
-  <div>index page</div>
+  <section class="section">
+    <h2 class="subtitle">曲/譜面を探す</h2>
+    <div class="columns is-multiline">
+      <section
+        v-for="m in menuList"
+        :key="m.title"
+        class="column is-half-tablet is-one-third-desktop is-one-quarter-widescreen"
+      >
+        <Card :title="m.title" variant="primary" collapsible>
+          <div class="card-content">
+            <div class="buttons">
+              <NuxtLink
+                v-for="i in m.items"
+                :key="i.name"
+                class="button"
+                type="is-text"
+                :to="i.to"
+              >
+                {{ i.name }}
+              </NuxtLink>
+            </div>
+          </div>
+        </Card>
+      </section>
+    </div>
+  </section>
 </template>
+
+<script lang="ts" setup>
+import Card from '~/components/Card.vue'
+import { courseSeriesIndexes, seriesNames, shortenSeriesName } from '~/src/song'
+
+const menuList = [
+  {
+    title: 'コースから探す',
+    items: courseSeriesIndexes.flatMap(series =>
+      ['NONSTOP', '段位認定'].map((s, i) => ({
+        name: `${s} (${shortenSeriesName(seriesNames[series])})`,
+        to: `/courses?type=${i + 1}&series=${series}`,
+      }))
+    ),
+  },
+]
+</script>
