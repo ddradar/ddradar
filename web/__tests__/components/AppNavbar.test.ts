08eff76499cd7c238de2410d5c03c94458797042
Nogic <24802730+nogic1008@users.noreply.github.com>
2023-03-30 03:54:07 +0000

    test(web): add AppNavbar component test

---
+++ web/__tests__/components/AppNavbar.test.ts
@@ -0,0 +1,48 @@
+import { publicUser } from '@ddradar/core/__tests__/data'
+import Oruga from '@oruga-ui/oruga-next'
+import { bulmaConfig } from '@oruga-ui/theme-bulma'
+import { RouterLinkStub } from '@vue/test-utils'
+import { beforeEach, describe, expect, test, vi } from 'vitest'
+
+import { mountAsync } from '~~/__tests__/test-utils'
+import AppNavbar from '~~/components/AppNavbar.vue'
+import useAuth from '~~/composables/useAuth'
+
+vi.mock('~~/composables/useAuth')
+
+describe('components/AppNavbar.vue', () => {
+  beforeEach(() => vi.mocked(useAuth).mockClear())
+
+  test('({ isLoggedin: false }) renders login button', async () => {
+    // Arrange
+    vi.mocked(useAuth).mockResolvedValue({ isLoggedIn: ref(false) } as any)
+
+    // Act
+    const wrapper = await mountAsync(AppNavbar, {
+      global: {
+        plugins: [[Oruga, bulmaConfig]],
+        stubs: { NuxtLink: RouterLinkStub },
+      },
+    })
+    expect(wrapper.element).toMatchSnapshot()
+  })
+  test('({ isLoggedin: true }) renders user name & logout button', async () => {
+    // Arrange
+    vi.mocked(useAuth).mockResolvedValue({
+      isLoggedIn: ref(true),
+      user: ref(publicUser),
+      name: ref(publicUser.name),
+    } as any)
+
+    // Act
+    const wrapper = await mountAsync(AppNavbar, {
+      global: {
+        plugins: [[Oruga, bulmaConfig]],
+        stubs: { NuxtLink: RouterLinkStub },
+      },
+    })
+
+    // Assert
+    expect(wrapper.element).toMatchSnapshot()
+  })
+})
