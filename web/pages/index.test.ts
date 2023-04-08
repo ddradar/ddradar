c9055b38b556a1aa2f6a3b404f1aa7174eb888af
nogic <24802730+nogic1008@users.noreply.github.com>
2023-04-08 23:47:09 +0900

    feat(web): add i18n on index page

--- web/pages/index.test.ts
+++ web/pages/index.test.ts
@@ -2,21 +2,25 @@ import Oruga from '@oruga-ui/oruga-next'
 import { bulmaConfig } from '@oruga-ui/theme-bulma'
 import { RouterLinkStub } from '@vue/test-utils'
 import { describe, expect, test } from 'vitest'
+import { createI18n } from 'vue-i18n'
 
 import Page from '~~/pages/index.vue'
 import { mountAsync } from '~~/test/test-utils'
 
 describe('Page /', () => {
-  test('renders expected menu', async () => {
-    // Arrange - Act
-    const wrapper = await mountAsync(Page, {
-      global: {
-        plugins: [[Oruga, bulmaConfig]],
-        stubs: { NuxtLink: RouterLinkStub },
-      },
-    })
+  describe.each(['ja', 'en'])('{ locale: "%s" } snapshot test', locale => {
+    const i18n = createI18n({ legacy: false, locale })
+    test('renders collectly', async () => {
+      // Arrange - Act
+      const wrapper = await mountAsync(Page, {
+        global: {
+          plugins: [[Oruga, bulmaConfig], i18n],
+          stubs: { NuxtLink: RouterLinkStub },
+        },
+      })
 
-    // Assert
-    expect(wrapper.element).toMatchSnapshot()
+      // Assert
+      expect(wrapper.element).toMatchSnapshot()
+    })
   })
 })
