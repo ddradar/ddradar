9a6b856b423ee7b17e083d8348f15efef4dc488d
Nogic <24802730+nogic1008@users.noreply.github.com>
2023-04-13 08:30:39 +0900

    test(web): add saveNotification test

--- web/pages/admin/notification/[[id]].test.ts
+++ web/pages/admin/notification/[[id]].test.ts
@@ -1,10 +1,18 @@
 import { notification } from '@ddradar/core/test/data'
-import Oruga from '@oruga-ui/oruga-next'
+import Oruga, { useProgrammatic } from '@oruga-ui/oruga-next'
 import { bulmaConfig } from '@oruga-ui/theme-bulma'
 import { describe, expect, test, vi } from 'vitest'
 
 import Page from '~~/pages/admin/notification/[[id]].vue'
 import { mountAsync } from '~~/test/test-utils'
+
+vi.mock('@oruga-ui/oruga-next', async origin => {
+  const actual = (await origin()) as typeof import('@oruga-ui/oruga-next')
+  return {
+    ...actual,
+    useProgrammatic: vi.fn(),
+  }
+})
 
 describe('Page /admin/notification', () => {
   describe('snapshot tests', () => {
@@ -24,6 +32,46 @@ describe('Page /admin/notification', () => {
 
       // Assert
       expect(wrapper.element).toMatchSnapshot()
+    })
+  })
+
+  // Method
+  describe('saveNotification', () => {
+    test('calls POST /api/v1/notification', () => {
+      // Arrange
+      vi.mocked(useProgrammatic).mockClear()
+      vi.mocked($fetch).mockClear()
+
+      const modalOpen = vi.fn()
+      modalOpen.mockReturnValue({ promise: Promise.resolve('yes') })
+      /* eslint-disable @typescript-eslint/no-explicit-any */
+      vi.mocked(useProgrammatic).mockReturnValue({
+        oruga: { modal: { open: modalOpen }, notification: { open: vi.fn() } },
+      } as any)
+      vi.mocked($fetch).mockResolvedValue({ ...notification } as any)
+      /* eslint-enable @typescript-eslint/no-explicit-any */
+
+      // Act
+      const wrapper = await mountAsync(Page, {
+        global: { plugins: [[Oruga, bulmaConfig]] },
+      })
+      const vm = wrapper.getComponent(Page).vm as unknown as {
+        saveNotification(): Promise<void>
+      }
+      await vm.saveNotification()
+
+      // Assert
+      vi.mocked($fetch).toBeCalledWith('/api/v1/notification', {
+        method: 'POST',
+        body: {
+          sender: 'SYSTEM',
+          pinned: false,
+          type: 'info',
+          icon: '',
+          title: '',
+          body: '',
+        },
+      })
     })
   })
 })
