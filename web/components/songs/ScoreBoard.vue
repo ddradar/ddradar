0b73987b4140e6d1a0ba428f345e2622a713c220
Nogic <24802730+nogic1008@users.noreply.github.com>
2023-06-11 08:35:14 +0000

    feat(web): add ScoreBord component

---
+++ web/components/songs/ScoreBoard.vue
@@ -0,0 +1,155 @@
+<template>
+  <CollapsibleCard
+    :title="getChartTitle(chart)"
+    :variant="cardType"
+    collapsible
+  >
+    <div class="card-content">
+      <div class="table-container">
+        <OTable
+          :data="scores"
+          :loading="pending"
+          :mobile-cards="false"
+          :selected="userScore"
+        >
+          <OTableColumn v-slot="r" field="name" :label="t('list.name')">
+            <NuxtLink
+              v-if="!r.row.isArea"
+              :to="`/users/${r.row.userId}`"
+              class="is-size-7"
+            >
+              {{ r.row.userName }}
+            </NuxtLink>
+            <span v-else class="is-size-7">{{
+              t(`area.${r.row.userId}`)
+            }}</span>
+          </OTableColumn>
+          <OTableColumn
+            v-slot="r"
+            field="score"
+            :label="t('list.score')"
+            centered
+          >
+            <ScoreBadge :lamp="r.row.clearLamp" :score="r.row.score" />
+          </OTableColumn>
+          <OTableColumn
+            v-slot="r"
+            field="exScore"
+            :label="t('list.exScore')"
+            numeric
+          >
+            <span class="is-size-7">{{ r.row.exScore }}</span>
+          </OTableColumn>
+
+          <template #empty>
+            <section class="section">
+              <div class="content has-text-grey has-text-centered">
+                <p>{{ t('list.noData') }}</p>
+              </div>
+            </section>
+          </template>
+        </OTable>
+      </div>
+    </div>
+    <footer class="card-footer">
+      <a v-if="isLoggedIn" class="card-footer-item">
+        {{ t('button.edit') }}
+      </a>
+      <a
+        v-if="isLoggedIn && !isPageDeletedOnGate(info.id)"
+        class="card-footer-item"
+      >
+        {{ t('button.import') }}
+      </a>
+      <a class="card-footer-item" @click="refresh()">
+        {{ t('button.all') }}
+      </a>
+    </footer>
+  </CollapsibleCard>
+</template>
+
+<i18n src="../../i18n/area.json"></i18n>
+<i18n lang="json">
+{
+  "ja": {
+    "list": {
+      "name": "ユーザー名",
+      "score": "スコア",
+      "exScore": "EX",
+      "noData": "データがありません",
+      "top": "{area}トップ"
+    },
+    "button": {
+      "edit": "編集",
+      "import": "インポート",
+      "all": "全件表示"
+    },
+    "area": {
+      "0": "全国"
+    }
+  },
+  "en": {
+    "list": {
+      "name": "Name",
+      "score": "Score",
+      "exScore": "EX",
+      "noData": "No Data",
+      "top": "{area} Top"
+    },
+    "button": {
+      "edit": "Edit",
+      "import": "Import",
+      "all": "Show All"
+    },
+    "area": {
+      "0": "World"
+    }
+  }
+}
+</i18n>
+
+<script lang="ts" setup>
+import { difficultyMap, isAreaUser, isPageDeletedOnGate } from '@ddradar/core'
+import { useI18n } from 'vue-i18n'
+
+import CollapsibleCard from '~~/components/CollapsibleCard.vue'
+import ScoreBadge from '~~/components/songs/ScoreBadge.vue'
+import useAuth from '~~/composables/useAuth'
+import type { CourseInfo } from '~~/server/api/v1/courses/[id].get'
+import type { SongInfo } from '~~/server/api/v1/songs/[id].get'
+import { getChartTitle } from '~~/utils/song'
+
+type CourseChart = CourseInfo['charts'][number]
+type Chart = SongInfo['charts'][number] | CourseChart
+
+interface ScoreBoardProps {
+  info: SongInfo | CourseInfo
+  chart: Chart
+}
+
+const props = defineProps<ScoreBoardProps>()
+const { t } = useI18n()
+const { id, isLoggedIn } = await useAuth()
+const fetchAllData = useState(() => false)
+const {
+  data: scores,
+  pending,
+  refresh,
+} = await useFetch(
+  `/api/v1/scores/${props.info.id}/${props.chart.playStyle}/${props.chart.difficulty}`,
+  {
+    query: computed(() => ({ scope: fetchAllData.value ? 'full' : 'medium' })),
+    /** c8 ignore next */
+    transform: rawScores =>
+      rawScores.map(s =>
+        isAreaUser({ id: s.userId }) ? { ...s, isArea: true } : s
+      ),
+  }
+)
+
+const cardType = computed(() =>
+  // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+  difficultyMap.get(props.chart.difficulty)!.toLowerCase()
+)
+const userScore = computed(() => scores.value?.find(s => s.userId === id.value))
+</script>
