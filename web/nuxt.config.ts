d6660455351baf0eaa17f73beb9887aefb0f9034
Nogic <24802730+nogic1008@users.noreply.github.com>
2024-02-08 08:45:28 +0900

    build: move swa config to nitro.azure.config

--- web/nuxt.config.ts
+++ web/nuxt.config.ts
@@ -3,17 +3,67 @@ import { defineNuxtConfig } from 'nuxt/config'
 export default defineNuxtConfig({
   nitro: {
     preset: 'azure',
-    prerender: {
-      ignore: ['/.auth/**', '/data-api/**'],
+    azure: {
+      config: {
+        auth: {
+          rolesSource: '/api/v1/user/roles',
+          identityProviders: {
+            twitter: {
+              registration: {
+                consumerKeySettingName: 'TWITTER_API_KEY',
+                consumerSecretSettingName: 'TWITTER_API_SECRET',
+              },
+            },
+            customOpenIdConnectProviders: {
+              line: {
+                registration: {
+                  clientIdSettingName: 'LINE_CLIENT_ID',
+                  clientCredential: {
+                    clientSecretSettingName: 'LINE_CLIENT_SECRET',
+                  },
+                  openIdConnectConfiguration: {
+                    wellKnownOpenIdConfiguration:
+                      'https://access.line.me/.well-known/openid-configuration',
+                  },
+                },
+                login: { nameClaimType: 'name' },
+              },
+            },
+          },
+        },
+        globalHeaders: {
+          'Access-Control-Allow-Origin': 'https://p.eagate.573.jp',
+          'Access-Control-Allow-Methods': 'POST, GET, OPTIONS',
+          'Access-Control-Allow-Credentials': 'true',
+        },
+        routes: [
+          {
+            route: '/api/v1/scores/*',
+            methods: ['POST', 'DELETE'],
+            allowedRoles: ['authenticated'],
+          },
+          {
+            route: '/api/v1/notification',
+            methods: ['POST'],
+            allowedRoles: ['administrator'],
+          },
+          {
+            route: '/api/v1/songs',
+            methods: ['POST'],
+            allowedRoles: ['administrator'],
+          },
+          { route: '/api/v1/user', allowedRoles: ['authenticated'] },
+          { route: '/api/v1/users/exists/*', allowedRoles: ['authenticated'] },
+          { route: '/admin/*', allowedRoles: ['administrator'] },
+        ],
+      },
     },
   },
   build: {
     transpile: [/@oruga-ui\/oruga-next/],
   },
   routeRules: {
-    '/.auth/**': { ssr: false },
     '/admin/**': { ssr: false },
-    '/data-api/**': { ssr: false },
   },
   app: {
     head: {
