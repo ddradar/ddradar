8e15aaf690feae106ad9a5e4a21e70412d380fa8
Nogic <24802730+nogic1008@users.noreply.github.com>
2024-02-28 11:58:17 +0900

    feat: migrate to @nuxt/test-utils

--- web/vitest.config.ts
+++ web/vitest.config.ts
@@ -1,36 +1,9 @@
-import fs from 'node:fs'
-import { resolve } from 'node:path'
+import { defineVitestConfig } from '@nuxt/test-utils/config'
 
-import VueI18nPlugin from '@intlify/unplugin-vue-i18n/vite'
-import vue from '@vitejs/plugin-vue'
-import { defineConfig } from 'vitest/config'
-
-const NuxtTsConfig = fs.readFileSync('./.nuxt/tsconfig.json').toString()
-const tsConfigFormated = JSON.parse(
-  NuxtTsConfig.replace(
-    /\\"|"(?:\\"|[^"])*"|(\/\/.*|\/\*[\s\S]*?\*\/)/g,
-    (m, g) => (g ? '' : m)
-  )
-)
-
-export const alias: Record<string, string> = {
-  '@ddradar/core/test/data': resolve(__dirname, '../core/test/data.ts'),
-  '@ddradar/core': resolve(__dirname, '../core/src'),
-  '@ddradar/db': resolve(__dirname, '../db/src'),
-}
-
-Object.entries(
-  tsConfigFormated.compilerOptions.paths as Record<string, string[]>
-).forEach(([key, value]) => {
-  alias[key] = resolve(__dirname, value[0])
-})
-
-export default defineConfig({
+export default defineVitestConfig({
   root: '.',
-  plugins: [vue(), VueI18nPlugin({ compositionOnly: true })],
-  resolve: { alias },
   test: {
-    environment: 'happy-dom',
+    environment: 'nuxt',
     setupFiles: './test/setup.ts',
     coverage: {
       enabled: true,
