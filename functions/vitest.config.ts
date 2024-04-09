fe281a25c54402dc82284d40a54000c2256ae0d9
Nogic <24802730+nogic1008@users.noreply.github.com>
2024-04-09 22:03:23 +0900

    fix(func): avoid use @ddradar/db

--- functions/vitest.config.ts
+++ functions/vitest.config.ts
@@ -7,7 +7,6 @@ export default defineConfig({
     alias: {
       '@ddradar/core/test/data': resolve(__dirname, '../core/test/data.ts'),
       '@ddradar/core': resolve(__dirname, '../core/src'),
-      '@ddradar/db': resolve(__dirname, '../db/src'),
     },
   },
   test: {
