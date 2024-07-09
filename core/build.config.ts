774aea048836c05fdaea35e065b9f06c9bf484f8
Nogic <24802730+nogic1008@users.noreply.github.com>
2024-07-09 09:27:06 +0900

    chore: setup typia

---
+++ core/build.config.ts
@@ -0,0 +1,27 @@
+import UnpluginTypia from '@ryoppippi/unplugin-typia/rollup'
+import { defineBuildConfig } from 'unbuild'
+
+export default defineBuildConfig({
+  entries: [
+    {
+      input: 'src/index',
+      format: 'esm',
+    },
+    {
+      input: 'src/',
+      format: 'cjs',
+      ext: 'cjs',
+    },
+    {
+      input: 'test/data',
+      format: 'esm',
+    },
+  ],
+  declaration: 'compatible',
+  hooks: {
+    'rollup:options'(_ctx, options) {
+      // @ts-expect-error Rollup versioning
+      options.plugins.push(UnpluginTypia())
+    },
+  },
+})
