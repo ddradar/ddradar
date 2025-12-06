84f80cf89692ece5ad21319a9664f757eda11189
Nogic <24802730+nogic1008@users.noreply.github.com>
2025-12-06 21:57:03 +0900

    feat: init with nuxthub
    https://hub.nuxt.com/docs/getting-started/installation#cli

---
+++ nuxt.config.ts
@@ -0,0 +1,35 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  // https://nuxt.com/modules
+  modules: [
+    '@nuxthub/core',
+    '@nuxt/eslint'
+  ],
+
+  // https://devtools.nuxt.com
+  devtools: { enabled: true },
+
+  // Env variables - https://nuxt.com/docs/getting-started/configuration#environment-variables-and-private-tokens
+  runtimeConfig: {
+    public: {
+      // Can be overridden by NUXT_PUBLIC_HELLO_TEXT environment variable
+      helloText: 'Hello from the Edge 👋'
+    }
+  },
+  // https://nuxt.com/docs/getting-started/upgrade#testing-nuxt-4
+  future: { compatibilityVersion: 4 },
+  compatibilityDate: '2025-03-01',
+
+  // https://hub.nuxt.com/docs/getting-started/installation#options
+  hub: {},
+
+  // Development config
+  eslint: {
+    config: {
+      stylistic: {
+        quotes: 'single',
+        commaDangle: 'never'
+      }
+    }
+  }
+})
