b949734a76540de4c65e27a3d6d00c5b9f230f05
Nogic <24802730+nogic1008@users.noreply.github.com>
2026-01-24 15:41:57 +0900

    feat: add WIP banner

--- app/app.vue
+++ app/app.vue
@@ -1,13 +1,30 @@
 <script setup lang="ts">
+import type { ButtonProps } from '@nuxt/ui'
 import { en, ja } from '@nuxt/ui/locale'
 
-const { locale } = useI18n()
+const { locale, t } = useI18n()
+const { github } = useRuntimeConfig().public.community
+const actions: ButtonProps[] = [
+  {
+    label: 'GitHub',
+    to: github,
+    target: '_blank',
+    icon: 'i-simple-icons-github',
+  },
+]
 
 const nuxtUILocales = computed(() => (locale.value === 'ja' ? ja : en))
 </script>
 
 <template>
   <UApp :locale="nuxtUILocales">
+    <UBanner
+      color="warning"
+      icon="i-lucide-info"
+      :title="t('error.wip')"
+      :actions="actions"
+      close
+    />
     <AppHeader />
     <UMain>
       <NuxtLayout>
